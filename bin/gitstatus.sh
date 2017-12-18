7416ef5fdfe0a52615a876b55fe9439633ea7878
caseneuve <piotr.kaznowski@gmail.com>
2017-12-18 16:22:03 +0100

    git status skrypt

--- bin/gitstatus.sh
+++ bin/gitstatus.sh
@@ -1,4 +1,15 @@
 #!/bin/bash
 
-cd /home/piotr/.dotfiles
-git status
+YEL=$(tput setaf 3)
+RED=$(tput setaf 1)
+RESET=$(tput sgr0)
+bold=$(tput bold)
+
+cd /home/piotr/.dotfiles/
+if [[ `git status --porcelain` ]]; then
+    clear
+    printf "${YEL}${bold}Git status - masz do opublikowania następujące zmiany:${RESET}\n$(git status --porcelain)\n-----------------------------------------------------\n\n$(git diff --cached)\n" 
+else
+    git status
+fi
+
