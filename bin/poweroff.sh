3ed024424df724352685f7977807ea9d3918c63b
caseneuve <piotr.kaznowski@gmail.com>
2017-12-18 09:56:02 +0100

    cd poweroff :-)

--- bin/poweroff.sh
+++ bin/poweroff.sh
@@ -1,11 +1,14 @@
 #!/bin/bash
 
+YEL=$(tput setaf 3)
 RED=$(tput setaf 1)
 RESET=$(tput sgr0)
 bold=$(tput bold)
 
 cd /home/piotr/.dotfiles/
     if [[ `git status --porcelain` ]]; then
+        clear
+        printf "${YEL}${bold}Git status - masz do opublikowania następujące zmiany:${RESET}\n$(git status --porcelain)\n\n"
         /home/piotr/.bin/gitpush.sh && \
         while true; do
         read -p "${bold}${RED}### Czy chcesz wyłączyć komputer? [t/n] ###${RESET} " yn
