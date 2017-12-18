d7b7e8eb7725a80c57974ac666e91c606d32f533
caseneuve <piotr.kaznowski@gmail.com>
2017-12-18 09:28:29 +0100

    cd3

--- bin/poweroff.sh
+++ bin/poweroff.sh
@@ -1,23 +1,28 @@
 #!/bin/bash
 
+RED="\[$(tput setaf 1)\]"
+RESET="\[$(tput sgr0)\]"
+
+
 cd /home/piotr/.dotfiles/
-if [[ `git status --porcelain` ]]; then
-    /home/piotr/.bin/gitpush.sh && \
+    if [[ `git status --porcelain` ]]; then
+        /home/piotr/.bin/gitpush.sh && \
+        while true; do
+        read -p "${RED}### Czy chcesz wyłączyć komputer? [t/n] ###${RESET} " yn
+        case $yn in
+            [Tt]* ) systemctl suspend;;
+            [Nn]* ) exit;;
+             * ) echo "Wpisz [Tt/Nn] :";;
+         esac
+    done   
+ 
+else
     while true; do
-    read -p "Czy chcesz wyłączyć komputer? [t/n] " yn
+    read -p "${RED}### Czy chcesz wyłączyć komputer? [t/n] ###${RESET} " yn
     case $yn in
         [Tt]* ) systemctl suspend;;
         [Nn]* ) exit;;
         * ) echo "Wpisz [Tt/Nn] :";;
     esac
 done   
- 
-else
-    echo "Do you wish to install this program?"
-    select yn in "Yes" "No"; do
-        case $yn in
-            Yes ) systemctl suspend;;
-            No ) exit;;
-        esac
-    done 
 fi
