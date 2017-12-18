06751c2283afe29faee2fc712cef9759cac33f03
caseneuve <piotr.kaznowski@gmail.com>
2017-12-18 09:31:32 +0100

    cd4

--- bin/poweroff.sh
+++ bin/poweroff.sh
@@ -1,16 +1,16 @@
 #!/bin/bash
 
-RED="\[$(tput setaf 1)\]"
-RESET="\[$(tput sgr0)\]"
-
+RED=[$(tput setaf 1)
+RESET=$(tput sgr0)
+bold=$(tput bold)
 
 cd /home/piotr/.dotfiles/
     if [[ `git status --porcelain` ]]; then
         /home/piotr/.bin/gitpush.sh && \
         while true; do
-        read -p "${RED}### Czy chcesz wyłączyć komputer? [t/n] ###${RESET} " yn
+        read -p "${bold}${RED}### Czy chcesz wyłączyć komputer? [t/n] ###${RESET} " yn
         case $yn in
-            [Tt]* ) systemctl suspend;;
+            [Tt]* ) systemctl suspend; break;;
             [Nn]* ) exit;;
              * ) echo "Wpisz [Tt/Nn] :";;
          esac
@@ -20,7 +20,7 @@ else
     while true; do
     read -p "${RED}### Czy chcesz wyłączyć komputer? [t/n] ###${RESET} " yn
     case $yn in
-        [Tt]* ) systemctl suspend;;
+        [Tt]* ) systemctl suspend; break;;
         [Nn]* ) exit;;
         * ) echo "Wpisz [Tt/Nn] :";;
     esac
