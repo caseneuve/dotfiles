9acef40c7425adee6b5c675fda25c56912d111c2
caseneuve <piotr.kaznowski@gmail.com>
2017-12-18 09:18:29 +0100

    poprawki do poweroff

--- bin/poweroff.sh
+++ bin/poweroff.sh
@@ -1,4 +1,15 @@
 #!/bin/bash
 
-/home/piotr/.bin/gitpush.sh && \
-systemctl poweroff -i
+cd /home/piotr/.dotfiles/
+if [[ `git status --porcelain` ]]; then
+    /home/piotr/.bin/gitpush.sh && \
+    systemctl suspend
+else
+    echo "Do you wish to install this program?"
+    select yn in "Yes" "No"; do
+        case $yn in
+            Yes ) systemctl suspend;;
+            No ) exit;;
+        esac
+    done 
+fi
