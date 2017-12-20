443845bd3bc18a64c6830f0baeeb9e46db7c7b2c
caseneuve <piotr.kaznowski@gmail.com>
2017-12-20 21:07:41 +0100

    skrypty popr

--- bin/gitpush.sh
+++ bin/gitpush.sh
@@ -4,9 +4,15 @@ bold=$(tput bold)
 cyan=$(tput setaf 6)
 reset=$(tput sgr0)
 
-cd /home/piotr/.dotfiles &&\
-git add . && \
-git add -u && \
-read -p "${cyan}${bold}Opis commita: ${reset}" opis && \
-git commit -m "$opis" &&\
-git push
+cd /home/piotr/.dotfiles && \
+
+if git diff-index --quiet HEAD --; then
+    echo "Git jest aktualny.";
+else
+    git add . && \
+    git add -u && \
+    read -p "${cyan}${bold}Opis commita: ${reset}" opis && \
+    git commit -m "$opis" && \
+    git push
+fi
+
