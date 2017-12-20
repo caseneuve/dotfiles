e66f3d7a0e65c1c927d6ab03da301bb5f4263de4
caseneuve <piotr.kaznowski@gmail.com>
2017-12-20 19:24:05 +0100

    skrypty linkuję do .dotfiles

--- i3/i3lock/lock.sh
+++ i3/i3lock/lock.sh
@@ -1,8 +1,14 @@
 #!/bin/bash
+
 scrot /tmp/screen.png
+
 #convert /tmp/screen.png -scale 10% -scale 1000% /tmp/screen.png
 #convert /tmp/screen.png -paint 3 -modulate 80 /tmp/screen.png
+
 convert /tmp/screen.png -paint 1.5 /tmp/screen.png
-[[ -f ~/.i3/i3lock/lock.png ]] && convert /tmp/screen.png  ~/.i3/i3lock/lock.png  -gravity center -composite -matte /tmp/screen.png
+
+[[ -f $HOME/.dotfiles/i3/i3lock/lock.png ]] && convert /tmp/screen.png  ~/.dotfiles/i3/i3lock/lock.png  -gravity center -composite -matte /tmp/screen.png
+
 mocp -P
+
 i3lock -u -e -i /tmp/screen.png
