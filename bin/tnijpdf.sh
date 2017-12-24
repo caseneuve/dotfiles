92014d6fb08b54f5539140442203360a127213b6
caseneuve <piotr.kaznowski@gmail.com>
2017-12-24 02:13:19 +0100

    poprawka w tnij: sprawdź czy pdftk jest zainstalowany

--- bin/tnijpdf.sh
+++ bin/tnijpdf.sh
@@ -6,5 +6,11 @@
 YEL='\033[1;33m' # yellow
 NC='\033[0m'     # no color
 
-pdftk "$1" cat $2 output $3
-printf "${YEL}### Utworzyłem plik: ###${NC}\n"; ls "$3"
+if pacman -Qs pdftk > /dev/null ; then
+    pdftk "$1" cat $2 output $3
+    printf "${YEL}### Utworzyłem plik: ###${NC}\n"; ls "$3";
+else
+    yaourt --noconfirm pdftk && \
+    pdftk "$1" cat $2 output $3
+    printf "${YEL}### Utworzyłem plik: ###${NC}\n"; ls "$3";
+fi
