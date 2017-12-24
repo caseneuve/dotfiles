29dbbf840634810874e3f8efbe4514b1dd355070
caseneuve <piotr.kaznowski@gmail.com>
2017-12-24 03:41:58 +0100

    manj popr

--- bin/poweroff.sh
+++ bin/poweroff.sh
@@ -9,7 +9,7 @@ cd /home/piotr/.dotfiles/
     if [[ `git status --porcelain` ]]; then
         clear
         printf "${YEL}${bold}Git status - masz do opublikowania następujące zmiany:${RESET}\n$(git status --porcelain)\n-----------------------------------------------------\n\n$(git diff --cached)\n" 
-        /home/piotr/.bin/gitpush.sh && \
+        /home/piotr/.dotfiles/bin/gitpush.sh && \
         while true; do
         printf "\n"
         read -p "${bold}${RED}### Czy chcesz wyłączyć komputer? [t/n] ###${RESET} " yn
