4cccb23623f4c178588f934edea15efd71e5464e
caseneuve <piotr.kaznowski@gmail.com>
2017-12-27 12:59:15 +0100

    gts popr

--- bin/gitstatus.sh
+++ bin/gitstatus.sh
@@ -8,7 +8,8 @@ bold=$(tput bold)
 cd /home/piotr/.dotfiles/
 if [[ `git status --porcelain` ]]; then
     clear
-    printf "${YEL}${bold}Git status - masz do opublikowania następujące zmiany:${RESET}\n$(git status --porcelain)\n-----------------------------------------------------\n\n$(git diff --cached)\n" 
+    printf "${YEL}${bold}Git status - masz do opublikowania następujące zmiany:${RESET}\n$(git status --porcelain)\n"
+#-----------------------------------------------------\n\n$(git diff --cached)\n" 
 else
     git status
 fi
