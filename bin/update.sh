c333e7e34b35eaf259e9c2368f56c2779cccd0a4
caseneuve <piotr.kaznowski@gmail.com>
2018-01-30 20:56:07 +0100

    zmiany w updade.sh (ranger/scope.sh) oraz w czyszczenie /i3

--- bin/update.sh
+++ bin/update.sh
@@ -2,24 +2,26 @@
 
 # ~/.dotfiles/bin/update.sh
 # Created:     15.01.18, 13:14    @x200
-# Last update: 29.01.18, 20:15:38 @manjaroi3
+# Last update: 30.01.18, 20:30:56 @manjaroi3
 
 # Doc:
-# - updates symbolic links for mutt
-# - installes ditaa, R
+# - updates symbolic links for mutt and ranger (scope.sh)
+# - installes ditaa, R, gnome-calc, docx2txt, unrtf
 
 # Status:
 # todo: wydzielić instalowanie paczek do innego skryptu
 
+# CSH:
+
+# variables
 file1=$HOME/.dotfiles/mutt/listenaudio.sh
 sym1=$HOME/.mutt/listenaudio.sh
 file2=$HOME/.dotfiles/mutt/goobook.sh
 sym2=$HOME/.mutt/goobook.sh
 mdir=$HOME/.mutt/
-
-
-# check if files are updated from git
-#$HOME/.dotfiles/bin/gitpull.sh
+file3=$HOME/.dotfiles/ranger/scope.sh
+sym3=$HOME/.config/ranger/scope.sh
+bak=$HOME/.config/bak/
 
 # check symlink mutt/listenaudio
 printf "Sprawdzam, czy istnieje link do skryptu:\n$file1...\n"
@@ -30,8 +32,8 @@ if ! [ -L $mdir/listenaudio.sh ]; then
 else
     echo "...OK!"
 fi
-# check symlink mutt/goobook
 
+# check symlink mutt/goobook
 printf "Sprawdzam, czy istnieje link do skryptu:\n$file2...\n"
 if ! [ -L $sym2 ]; then
     echo "Tworzę symlink dla $file2."
@@ -41,11 +43,29 @@ else
     echo "...OK!"
 fi
 
+# check symlink ranger/scope.sh
+printf "Sprawdzam, czy istnieje link do skryptu:\n$file3...\n"
+if ! [ -L $sym3 ]; then
+    if [ -d $bak ]; then
+        mv $file3 $bak
+    else
+        mkdir $bak
+        mv $file3 $bak
+    fi
+    echo "Tworzę symlink dla $file3."
+    ln -s $file3 $sym3
+    echo "...OK!"
+else
+    echo "...OK!"
+fi
+
+
+# check if packages are installed
 echo "Sprawdzam paczki..."
 /home/piotr/.dotfiles/Boot-ex/goobook.sh
 
-# zainstaluj ditaa i R
-for package in ditaa r gnome-calculator
+# zainstaluj brakujące paczki
+for package in ditaa r gnome-calculator unrtf docx2txt calcurse
 do
 if pacman -Qi $package &> /dev/null ; then
   echo "Paczka $(pacman -Q $package) jest zainstalowana." 
