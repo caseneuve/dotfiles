42a9b588e3132a6ea51f98e2cfd69d8473d2ffe7
caseneuve <piotr.kaznowski@gmail.com>
2018-02-03 14:32:51 +0100

    mutt/viepdf

--- mutt/viewpdf.sh
+++ mutt/viewpdf.sh
@@ -1,3 +1,7 @@
 #!/bin/bash
 
-i3-msg 'workspace 5: ' && mupdf "$1" && i3-msg workspace number 10
+# Last update: 02.02.18, 15:39:04 @x200
+
+# Doc: skrypt wykonawczy dla mutt'a
+
+i3-msg 'workspace 5' && mupdf "$1" && i3-msg workspace number 10
