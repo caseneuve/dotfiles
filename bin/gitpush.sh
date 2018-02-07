dd450340e41c48669e792e3b9a60e7a6de779d43
caseneuve <piotr.kaznowski@gmail.com>
2018-02-08 00:49:21 +0100

    git push (warunek dla gitlab)

--- bin/gitpush.sh
+++ bin/gitpush.sh
@@ -2,7 +2,7 @@
 
 # ~/.dotfiles/bin/asdf.sh
 # Created:     27.12.17           ?
-# Last update: 04.02.18, 20:51:37 @x200
+# Last update: 08.02.18, 00:48:56 @lenovo
 
 # Doc:
 #
@@ -22,10 +22,10 @@ if [ -z "$(git status --porcelain)" ]; then
     echo "Git jest aktualny.";
 else
     git add . && \
-    git add -u && \
-    read -p "${cyan}${bold}Opis commita: ${reset}" opis && \
-    git commit -m "$opis" && \
-    git push
+        git add -u && \
+        read -p "${cyan}${bold}Opis commita: ${reset}" opis && \
+        git commit -m "$opis" && \
+        git push
 fi
 printf "\n"
 
@@ -39,27 +39,29 @@ if [ -z "$(git status --porcelain)" ]; then
     echo "Git jest aktualny.";
 else
     git add . && \
-    git add -u && \
-    read -p "${cyan}${bold}Opis commita: ${reset}" opis && \
-    git commit -m "$opis" && \
-    git push
+        git add -u && \
+        read -p "${cyan}${bold}Opis commita: ${reset}" opis && \
+        git commit -m "$opis" && \
+        git push
 fi
 printf "\n"
 
 # repo Liberal Artist @gitlab
-echo "Aktualizuję repo LIBERAL ARTIST:"
-echo "======================="
-cd /home/piotr/gitlab/liberal_artist_org
-# ustaw cache na 30 min. (timeout=sec.), exit: "git credential-cache exit"
-git config credential.helper 'cache --timeout=1800'
-if [ -z "$(git status --porcelain)" ]; then
-    echo "Git jest aktualny.";
-else
-    git add . && \
-    git add -u && \
-    read -p "${cyan}${bold}Opis commita: ${reset}" opis && \
-    git commit -m "$opis" && \
-    git push
+if [ -d "/home/piotr/gitlab/liberal_artist_org" ]; then
+    echo "Aktualizuję repo LIBERAL ARTIST:"
+    echo "======================="
+    cd /home/piotr/gitlab/liberal_artist_org
+    # ustaw cache na 30 min. (timeout=sec.), exit: "git credential-cache exit"
+    git config credential.helper 'cache --timeout=1800'
+    if [ -z "$(git status --porcelain)" ]; then
+        echo "Git jest aktualny.";
+    else
+        git add . && \
+            git add -u && \
+            read -p "${cyan}${bold}Opis commita: ${reset}" opis && \
+            git commit -m "$opis" && \
+            git push
+    fi
 fi
 
 printf "\n"
