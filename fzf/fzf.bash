88b1b55eaed2d019ac7792c24ccf418c69463fbc
caseneuve <piotr.kaznowski@gmail.com>
2018-03-29 11:03:07 +0200

    aliasy &  updt

---
+++ fzf/fzf.bash
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+# Path:        ~/.dotfiles/fzf/fzf.bash
+# Created:     29.03.18, 11:02    @manjaroi3
+# Last update: 29.03.18, 11:02:36 @manjaroi3
+
+# Doc: functions for fuzzy finder
+
+
