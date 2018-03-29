471a30a56edabeb68dba01e1ab13d3871e34cf81
caseneuve <piotr.kaznowski@gmail.com>
2018-03-29 15:31:08 +0200

    fzf cd

--- fzf/fzf.bash
+++ fzf/fzf.bash
@@ -1,8 +1,8 @@
 # Path:        ~/.dotfiles/fzf/fzf.bash
 # Created:     29.03.18, 11:02    @manjaroi3
-# Last update: 29.03.18, 13:59:34 @manjaroi3
+# Last update: 29.03.18, 14:46:28 @manjaroi3
 
-# Doc: functions for fuzzy finder
+# Doc: Config & functions for FUZZY FINDER
 
 # Setup fzf
 # ---------
@@ -17,25 +17,40 @@ fi
 # Key bindings
 # ------------
 source "/home/piotr/.fzf/shell/key-bindings.bash"
+# Alt-c
+export FZF_ALT_C_COMMAND='fd -H --type d . ~'
 
-# default options
+# Default options
+# --------------
 export FZF_DEFAULT_OPTS='--height 50% --reverse --border --inline-info'
 export FZF_DEFAULT_COMMAND='fd --type f . ~'
 export FZF_COMPLETION_TRIGGER='~~'
 
-# Alt-c
-export FZF_ALT_C_COMMAND='fd --type d . ~'
+# Aliasy
+# ------
+# przejrzyj zawartość plików tekstowych w danym katalogu
+alias ffp="fd --type f -E '*.pdf' -E '*.mp?' -E '*.pyc' -E '*.RData' -E '*.png' . ./ | fzf --reverse --height 100% --border --inline-info --preview 'head -100 {}'"
 
 # FUNCTIONS
+# ---------
+
+# find pdf and open with preferred app (mupdf)
 fp () {
     xdg-open "$(fd -e pdf . ~ | fzf --height 50% --reverse --border --inline-info)" &>/dev/null
 }
 
+# find music file and play it with mocp
 fm () {
     mocp -l "$(fd -e mp3 . ~ | fzf --height 50% --reverse --border --inline-info)" &>/dev/null
 }
 
+# find file and open it in emacsclient (don't look for multimedia or non textual files)
 fe () {
     emacsclient -nw "$(fd --type f . ~ -E '*.mp?' -E '*.jpg' -E '*.png' -E '*.pdf' -E '*.aux' -E '*.doc*' -E '*.out' -E '*.mkv' | fzf --height 50% --reverse --border --inline-info)" 
 } 
+
+# find object and copy it to clipboard 
+fx () {
+    fzf | xclip -selection c
+}
 
