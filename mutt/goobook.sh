4ff713fa46a5176fd06492bb0d4339513d333d3a
caseneuve <piotr.kaznowski@gmail.com>
2018-04-04 21:18:58 +0200

    bla

--- mutt/goobook.sh
+++ mutt/goobook.sh
@@ -1,8 +1,9 @@
 # ~/.mutt/goobook.sh
 # Created:     15.01.18, 13:04    @x200
-# Last update: 15.01.18, 13:09:13 @x200
+# Last update: 04.04.18, 20:35:35 @manjaroi3
 
 # Doc: adds sender to google contacts, reloads goobook
 
 #!/bin/bash
 goobook add $1 && goobook reload
+
