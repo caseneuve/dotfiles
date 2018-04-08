108baae858953fcb50d5767835292283efbb9427
caseneuve <piotr.kaznowski@gmail.com>
2018-04-08 14:44:09 +0200

    notes.sh: append system name to date (dp unification)

--- bin/notes.sh
+++ bin/notes.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 
-# Last update: 28.01.18, 17:13:11 @x200
+# Last update: 08.04.18, 12:52:46 @manjaroi3
 
 # Doc:
 # kasuje stary notes i otwiera czysty do nadpisania, który będzie 
 # istniał (jeśli zapisany) do następnego wywołania komendy
 
 #rm ~/notes.txt
-printf "* $(date)\n" >> ~/notes.txt
+printf "* $(date) @$HOSTNAME\n" >> ~/notes.txt
 emacsclient -nw +1000 ~/notes.txt
 
