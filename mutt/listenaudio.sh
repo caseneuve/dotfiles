29891c6f155a46d68b4204f30d58f7f1dc4e3207
caseneuve <piotr.kaznowski@gmail.com>
2018-04-10 16:01:56 +0200

    mutt → listenaudio otwiera teraz zał. mp3 w mocp

--- mutt/listenaudio.sh
+++ mutt/listenaudio.sh
@@ -1,8 +1,11 @@
-# Last update: 15.01.18, 12:47:13 @x200
+# Last update: 10.04.18, 16:01:48 @manjaroi3
 
 #!/bin/bash
 
-clear;
-mpv $1
+#clear;
+#mpv $1 & 
+cp $1 /tmp/mutt_music.mp3 &&\
+mocp -l /tmp/mutt_music.mp3
 
 #xfce4-terminal -T mutt-audio -e "bash -c \"mpv $1; bash\"" - nie działa (nie widzi pliku w /tmp/)
+
