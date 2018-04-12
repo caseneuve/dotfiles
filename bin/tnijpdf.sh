ce1cebabc4afb2cf2c32da315bcc134afe7e452a
caseneuve <piotr.kaznowski@gmail.com>
2018-04-12 11:50:51 +0200

    tnij.sh BUG

--- bin/tnijpdf.sh
+++ bin/tnijpdf.sh
@@ -1,17 +1,24 @@
 #!/bin/bash
-# Last update: 05.02.18, 11:22:54 @x200
+# Last update: 12.04.18, 11:47:13 @x200
 
 # Doc:
 # fixme: nie działa dla stron wybranych nie po kolei (np. 12;15-18)
+# fixme: jak sprawdzić, czy plik powstał???
 
 YEL='\033[1;33m' # yellow
 NC='\033[0m'     # no color
+RED='\033[0;31m'
 
-if pacman -Qs pdftk > /dev/null ; then
-    pdftk "$1" cat $2 output $3
-    printf "${YEL}### Utworzyłem plik: ###${NC}\n"; ls "$3";
-else
-    yaourt --noconfirm pdftk && \
-    pdftk "$1" cat $2 output $3
-    printf "${YEL}### Utworzyłem plik: ###${NC}\n"; ls "$3";
+if ! pacman -Qs pdftk > /dev/null ; then
+    yaourt --noconfirm pdftk
 fi
+
+pdftk "$1" cat $2 output $3 & >/dev/null
+#if [ -f "$3" ]; then echo "ok"; else echo "$3 nie istnieje"; fi
+
+#if  ls "$(echo "$3")" ; then
+printf "${YEL}### Utworzyłem plik: ###${NC}\n$3\n"
+#else
+#    printf "${RED}### BŁĄD: ###${NC}\n Nie utworzyłem pliku $3\n"
+#fi
+
