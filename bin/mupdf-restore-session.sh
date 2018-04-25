7ba1e01c2358083615e2c99e8b841373651a610f
caseneuve <piotr.kaznowski@gmail.com>
2018-04-25 22:47:27 +0200

    mupdf-restore → log gdy nie ma pliku

--- bin/mupdf-restore-session.sh
+++ bin/mupdf-restore-session.sh
@@ -2,13 +2,22 @@
 
 # Path:        ~/.bin/mupdf-restore-session.sh
 # Created:     25.04.18, 21:04    @x200
-# Last update: 25.04.18, 21:13:43 @x200
+# Last update: 25.04.18, 22:41:23 @manjaroi3
 
 # Doc:
 
 cd ~/.cache
-sed '/^$/d; /#/d' .mupdf_cache | while read line ;
-do
-    [[ -f $line ]] && mupdf "$line" > /dev/null 2>&1 &
-done 
+if [[ -f .mupdf_cache ]]; then
+    sed '/^$/d; /#/d' .mupdf_cache | while read line ;
+    do
+        if [[ -f $line ]]; then
+            mupdf "$line" > /dev/null 2>&1 &
+        else
+            echo -e "Plik '$line' nie istnieje!"
+            echo -e "$(date +%Y-%m-%d,%H:%M) - $HOSTNAME\nPlik '$line' nie istnieje!"  >> ~/Dropbox/config/mupdf/mupdf-restore.log  
+        fi
+    done
+else
+    echo "Nie ma zachowanych sesji mupdf."
+fi
 
--- bin/mupdf_restore.sh
+++ bin/mupdf_restore.sh
@@ -2,7 +2,7 @@
 
 # Path:        ~/.dotfiles/bin/mupdf_restore.sh
 # Created:     25.04.18, 21:38    @x200
-# Last update: 25.04.18, 22:33:22 @manjaroi3
+# Last update: 25.04.18, 22:45:00 @manjaroi3
 
 # Doc:
 
@@ -14,6 +14,7 @@ do
         mupdf "$line" > /dev/null 2>&1 &
     else
         echo -e "Plik '$line' nie istnieje!"
+        echo -e "$(date '+%Y-%m-%d, %H:%M') - $HOSTNAME:\nPlik '$line' nie istnieje!"  >> ~/Dropbox/config/mupdf/mupdf-restore.log  
     fi
 done
 
