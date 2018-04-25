101cfb0f3177010390e88c0bce0a75b377a79e39
caseneuve <piotr.kaznowski@gmail.com>
2018-04-25 22:33:49 +0200

    mupdf restore → popr. warunek, gdy plik nie istnieje

--- bin/mupdf_restore.sh
+++ bin/mupdf_restore.sh
@@ -2,12 +2,18 @@
 
 # Path:        ~/.dotfiles/bin/mupdf_restore.sh
 # Created:     25.04.18, 21:38    @x200
-# Last update: 25.04.18, 21:39:03 @x200
+# Last update: 25.04.18, 22:33:22 @manjaroi3
 
 # Doc:
 
+clear
+
 sed '/^$/d; /#/d' $1 | while read line ;
 do
-    [[ -f $line ]] && mupdf "$line" > /dev/null 2>&1 &
+    if [[ -f $line ]]; then
+        mupdf "$line" > /dev/null 2>&1 &
+    else
+        echo -e "Plik '$line' nie istnieje!"
+    fi
 done
 
