8c7aced056b932447c533cdc5c1ac47426ee9e70
caseneuve <piotr.kaznowski@gmail.com>
2018-04-30 14:39:30 +0200

    mount.sh → clear message

--- bin/mount.sh
+++ bin/mount.sh
@@ -4,5 +4,6 @@ bold=$(tput bold)
 normal=$(tput sgr0)
 
 sudo mount /dev/sdb1 /media && \
+clear
 printf "${bold}Zamontowałem USB:${normal}\n\nNAME    MAJ:MIN RM   SIZE RO TYPE MOUNTPOINT\n--------------------------------------------\n$(lsblk | grep "sdb1")\n\n"
 ls -a --color=auto --group-directories-first /media
