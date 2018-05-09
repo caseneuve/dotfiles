10d268fa5c10839560b188c04c7b197aacbab59a
caseneuve <piotr.kaznowski@gmail.com>
2018-05-09 11:53:55 +0200

    sendmail quick korekta

--- bin/sendmailquick.sh
+++ bin/sendmailquick.sh
@@ -21,7 +21,7 @@ if [ -z "$1" ]; then
     echo "${red}${bold}Nie zaznaczyłeś załączników!${normal}";
 else
     echo -e "Quickmail sent via RANGER, $(date '+%d/%m/%Y, %H:%M')\nDo: $mailaddress\nAtt: $@\n" > ~/.mail/ranger_quicksent-$(date +%y%m%dT%H%M)
-    echo -e "Wiadomość wysłana automatycznie $(date '+%d/%m o %H:%M'), zawiera następujące pliki\n<< $@ >> \n--\n" | mutt -s "ranger" $mailaddress -a "$@" > /dev/null 2>&1 &&\
+    echo -e "Wiadomość wysłana automatycznie $(date '+%d/%m o %H:%M'), zawiera następujące pliki\n<< $@ >> \n\n" | mutt -s "ranger" $mailaddress -a "$@" > /dev/null 2>&1 &&\
         echo -e "\n${gre}Wysłałem wiadomość do ${normal}${bold}$mailaddress${normal}${gre} z następującymi załącznikami:\n'$@'${normal}\n"
 fi
 
