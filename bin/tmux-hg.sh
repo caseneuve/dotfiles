a374efa7b572d48fac14ffd14e90d6a952d5ceee
caseneuve <piotr.kaznowski@gmail.com>
2018-05-13 22:43:13 +0200

    tmux → start session with hg & calcurse

--- bin/tmux-hg.sh
+++ bin/tmux-hg.sh
@@ -2,12 +2,14 @@
 
 # Path:        ~/.tmux/tmux-hg.sh
 # Created:     13.03.18, 10:30    @x200
-# Last update: 13.03.18, 10:44:50 @x200
+# Last update: 13.05.18, 21:22:51 @x200
 
-# Doc: starts tmux with 2 windows, the second one named "hg" with hangups running
+# Doc: starts tmux with 3 windows, the second one named "hg" with HANGUPS and the third "cal" with CALCURSE running
 # https://stackoverflow.com/questions/5609192/how-to-set-up-tmux-so-that-it-starts-up-with-specified-windows-opened
 
-tmux new-session -d 
-tmux new-window -n 'hg' 'hangups' 
+tmux new-session -s 'tmux' -d 
+tmux new-window -n 'hg' 'hangups'
+tmux new-window -n 'cal' 'calcurse'
+tmux select-window -t 'tmux:cal'
 tmux -2 attach -d
 
