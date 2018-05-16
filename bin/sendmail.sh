e34f751e8f5844a13bdaa775fa1978b7b2f5e4a4
caseneuve <piotr.kaznowski@gmail.com>
2018-05-16 22:06:27 +0200

    sendmail.sh → poprawki + get mail addres from mutt aliases

--- bin/sendmail.sh
+++ bin/sendmail.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-# Last update: 10.05.18, 18:35:36 @lenovo
+# Last update: 16.05.18, 22:05:44 @manjaroi3
 
 # Doc:
 
@@ -10,6 +10,7 @@ yel=$(tput setaf 11)
 gre=$(tput setaf 10)
 red=$(tput setaf 1)
 blu=$(tput setaf 4)
+adbook='/home/piotr/Dropbox/config/.mutt_aliases'
 
 ## code:
 clear
@@ -18,24 +19,31 @@ echo "${yel}${bold}### Executing _send mail_ script ${normal}"
 echo "##  Multiline in body: \\\n"
 echo
 ## read:
-read -p "${bold}Do kogo chcesz wysłać wiadomość?${normal}${blu} " mailaddress
+read -p "${bold}Do kogo chcesz wysłać wiadomość?${normal}${blu} " alias
 read -p "${normal}${bold}Temat wiadomości:${normal}${blu}                " subject
 read -p "${normal}${bold}Treść wiadomości:${normal}${blu}                " body
 
+## get the email address
+if [[ $alias == *@* ]]; then
+    mailddrs=$alias
+else
+    mailddrs=$(grep "alias $alias " $adbook | grep -o "[a-z0-9\.\_]*@[a-z0-9\.]*")
+fi
+
 ## save mail in ~/.mail/...
 if [ -z "$1" ]; then
-    echo -e "Mail sent via RANGER, $(date '+%d/%m/%Y, %H:%M:%S')\nDo: $mailaddress\nTemat: $subject\nAtt: - \nTreść:\n<<\n$body\n>>\n" > ~/.mail/ranger_sent-$(date +%y%m%dT%H%M%S)
+    echo -e "Mail sent via RANGER, $(date '+%d/%m/%Y, %H:%M:%S')\n------------------------------------------\nDo:    $mailddrs\nTemat: $subject\nAtt:   - \n<-----------------------------------------\n\n$body\n\n----------------------------------------->\n" > ~/.mail/ranger_sent-$(date +%y%m%dT%H%M%S)
 else
-    echo -e "Mail sent via RANGER, $(date '+%d/%m/%Y, %H:%M:%S')\nDo: $mailaddress\nTemat: $subject\nAtt: $@\nTreść:\n<<\n$body\n>>\n" > ~/.mail/ranger_sent-$(date +%y%m%dT%H%M%S)
+    echo -e "Mail sent via RANGER, $(date '+%d/%m/%Y, %H:%M:%S')\n------------------------------------------\nDo:    $mailddrs\nTemat: $subject\nAtt:   $@\n<-----------------------------------------\n\n$body\n\n----------------------------------------->" > ~/.mail/ranger_sent-$(date +%y%m%dT%H%M%S)
 fi
 
 ## send mail via mutt:
 if [ -z "$1" ]; then
-    echo -e "${normal}\n...wysyłam"; echo -e "$body" | mutt -s "$subject" $mailaddress > /dev/null 2>&1 &&\
-        echo -e "\n${gre}Wysłałem wiadomość do ${normal}${bold}$mailaddress${normal}${gre} (bez załączników)$1${normal}\n"
+        echo -e "${normal}\n...wysyłam"; echo -e "$body" | mutt -s "$subject" $mailddrs > /dev/null 2>&1 &&\
+            echo -e "\n${gre}Wysłałem wiadomość do ${normal}${bold}$mailddrs${normal}${gre} (bez załączników)$1${normal}\n"
 else
-    echo -e "${normal}\n...wysyłam"; echo -e "$body" | mutt -s "$subject" $mailaddress -a "$@" > /dev/null 2>&1 &&\
-        echo -e "\n${gre}Wysłałem wiadomość do ${normal}${bold}$mailaddress${normal}${gre} z następującymi załącznikami:\n$@${normal}\n"
+    echo -e "${normal}\n...wysyłam"; echo -e "$body" | mutt -s "$subject" $mailddrs -a "$@" > /dev/null 2>&1 &&\
+        echo -e "\n${gre}Wysłałem wiadomość do ${normal}${bold}$mailddrs${normal}${gre} z następującymi załącznikami:\n${red}$@${normal}\n"
 fi
 
 sleep 3s
--- bin/sendmailquick.sh
+++ bin/sendmailquick.sh
@@ -7,22 +7,31 @@ yel=$(tput setaf 11)
 gre=$(tput setaf 10)
 red=$(tput setaf 1)
 blu=$(tput setaf 4)
+adbook='/home/piotr/Dropbox/config/.mutt_aliases'
 
 ## code:
 clear
 
 echo "${yel}${bold}### Executing _tnijpdf_ script ${normal}"
 echo
-read -p "${bold}Do kogo chcesz wysłać wiadomość? ${normal}${blu}" mailaddress
+read -p "${bold}Do kogo chcesz wysłać wiadomość? ${normal}${blu}" alias
 #read -p "Temat wiadomości: " subject
 #read -p "Treść wiadomości: " body
 
+## get the email address
+if [[ $alias == *@* ]]; then
+    mailddrs=$alias
+else
+    mailddrs=$(grep "alias $alias " $adbook | grep -o "[a-z0-9\.\_]*@[a-z0-9\.]*")
+fi
+
+# save mail in ~/.mail/...
 if [ -z "$1" ]; then
     echo "${red}${bold}Nie zaznaczyłeś załączników!${normal}";
 else
-    echo -e "Quickmail sent via RANGER, $(date '+%d/%m/%Y, %H:%M')\nDo: $mailaddress\nAtt: $@\n" > ~/.mail/ranger_quicksent-$(date +%y%m%dT%H%M)
-    echo -e "Wiadomość wysłana automatycznie $(date '+%d/%m o %H:%M'), zawiera następujące pliki\n<< $@ >> \n\n" | mutt -s "ranger" $mailaddress -a "$@" > /dev/null 2>&1 &&\
-        echo -e "\n${gre}Wysłałem wiadomość do ${normal}${bold}$mailaddress${normal}${gre} z następującymi załącznikami:\n'$@'${normal}\n"
+    echo -e "Quickmail sent via RANGER, $(date '+%d/%m/%Y, %H:%M:%S')\n-----------------------------------------------\nDo:     $mailddrs\nAtt:    $@\n" > ~/.mail/ranger_quicksent-$(date +%y%m%dT%H%M)
+    echo -e "Wiadomość wysłana automatycznie $(date '+%d/%m o %H:%M'), zawiera następujące pliki\n<< $@ >> \n\n" | mutt -s "ranger" $mailddrs -a "$@" > /dev/null 2>&1 &&\
+        echo -e "\n${gre}Wysłałem wiadomość do ${normal}${bold}$mailddrs${normal}${gre} z następującymi załącznikami:\n'$@'${normal}\n"
 fi
 
 sleep 3s
