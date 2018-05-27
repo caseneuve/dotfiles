fa9a94fb39360da4b01dcb045b3768fc9a5032a2
caseneuve <piotr.kaznowski@gmail.com>
2018-05-27 19:49:08 +0200

    emacs launcher, rnm qtb launcher

---
+++ bin/i3-emacs-launcher.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+# Path:        ~/.dotfiles/bin/i3-emacs-launcher.sh
+# Created:     27.05.18, 09:16    @x200
+# Last update: 27.05.18, 09:17:22 @x200
+
+# Doc: If emacs is running goes to the first workspace with qtb window; if not -- launches qtb at wksp 1
+
+ws=$(i3-msg -t get_tree | grep -oE "num\":[0-9]*||emacs" | tr "\n" " " | grep -o "num\":[0-9]* emacs" | grep -o [0-9]* | head -n 1)
+
+[[ "$ws" ]] && i3-msg "workspace $ws" || i3-msg "workspace 1; exec --no-startup-id emacs"
+
