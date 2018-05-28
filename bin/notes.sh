a3758020b07833da91583995b4e68eb1ba8060b5
caseneuve <piotr.kaznowski@gmail.com>
2018-05-28 11:10:28 +0200

    more lines (1000 is not enough)

--- bin/notes.sh
+++ bin/notes.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
-# Last update: 08.04.18, 15:09:21 @x200
+# Last update: 28.05.18, 11:09:40 @x200
 # Doc: dodaje wpis do notesu z dp, który jest symlinkowany do każdej maszyny
 
 printf "* $(date) @$HOSTNAME -- " >> ~/notes.txt
-emacsclient -nw +1000 ~/notes.txt
+emacsclient -nw +10000 ~/notes.txt
 
