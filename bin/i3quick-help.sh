150ef3c33075c72762848578fe2850cec3766fc0
caseneuve <piotr.kaznowski@gmail.com>
2018-05-29 08:39:48 +0200

    add print screen info

--- bin/i3quick-help.sh
+++ bin/i3quick-help.sh
@@ -2,7 +2,7 @@
 
 # Path:        ~/.dotfiles/i3quick-help.sh
 # Created:     20.05.18, 10:52    @lenovo
-# Last update: 20.05.18, 11:03:59 @lenovo
+# Last update: 29.05.18, 08:39:28 @x200
 
 # Doc: F1
 
@@ -14,5 +14,7 @@ launch a program = Wid+d
 -------
 video mode = mod+shift+v
 sticky toggle = mod+v
-floating toggle = mod+shift+space"
+floating toggle = mod+shift+space
+print screen = PrtSc
+print only current app = mod+PrtSc"
 
