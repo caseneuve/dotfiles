1ab5c2122e5ae4351d9b3298db9df3d0cbb80863
caseneuve <piotr.kaznowski@gmail.com>
2018-05-30 20:56:06 +0200

    + new pane → htop

--- bin/tmux-starter.sh
+++ bin/tmux-starter.sh
@@ -2,7 +2,7 @@
 
 # Path:        ~/.tmux/tmux-hg.sh
 # Created:     13.03.18, 10:30    @x200
-# Last update: 20.05.18, 22:48:17 @x200
+# Last update: 30.05.18, 20:51:47 @x200
 
 # Doc: starts tmux with 3 windows, the second one named "hg" with HANGUPS and the third "cal" with CALCURSE running
 # https://stackoverflow.com/questions/5609192/how-to-set-up-tmux-so-that-it-starts-up-with-specified-windows-opened
@@ -12,6 +12,7 @@ tmux new-window -n 'hg' 'hangups'
 tmux new-window -n 'rng' 'ranger'
 tmux new-window -n 'cal' 'calcurse'
 tmux new-window -n 'mocp' 'mocp'
+tmux new-window -n 'htop' 'htop'
 tmux rename-window -t%0 'term'
 tmux select-window -t 'tmux:cal'
 tmux -2 attach -d
