71229d0404dd372eeaf167b1711cabb9551b7568
caseneuve <piotr.kaznowski@gmail.com>
2018-06-11 07:18:06 +0200

    tmux starte → term : split, clock mode right

--- bin/tmux-starter.sh
+++ bin/tmux-starter.sh
@@ -2,7 +2,7 @@
 
 # Path:        ~/.tmux/tmux-hg.sh
 # Created:     13.03.18, 10:30    @x200
-# Last update: 30.05.18, 20:51:47 @x200
+# Last update: 11.06.18, 07:17:12 @x200
 
 # Doc: starts tmux with 3 windows, the second one named "hg" with HANGUPS and the third "cal" with CALCURSE running
 # https://stackoverflow.com/questions/5609192/how-to-set-up-tmux-so-that-it-starts-up-with-specified-windows-opened
@@ -14,6 +14,10 @@ tmux new-window -n 'cal' 'calcurse'
 tmux new-window -n 'mocp' 'mocp'
 tmux new-window -n 'htop' 'htop'
 tmux rename-window -t%0 'term'
+tmux select-window -t 'tmux:term'
+tmux splitw -h -p 30
+tmux clock-mode
+tmux select-pane -L
 tmux select-window -t 'tmux:cal'
 tmux -2 attach -d
 
