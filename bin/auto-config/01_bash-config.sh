bb996498bdda5e24d2b77725f790c50e32bb1af3
caseneuve <piotr.kaznowski@gmail.com>
2018-06-16 23:47:56 +0200

    auto-config → 01 source erratum

--- bin/auto-config/01_bash-config.sh
+++ bin/auto-config/01_bash-config.sh
@@ -2,7 +2,7 @@
 
 # Path:        ~/.dotfiles/bin/auto-config/bash-config.sh
 # Created:     16.06.18, 21:32    @lenovo
-# Last update: 16.06.18, 23:31:21 @lenovo
+# Last update: 16.06.18, 23:47:47 @lenovo
 
 # Doc: When repo /dotfiles is cloned, symlink bash files
 
@@ -19,4 +19,5 @@ for x in $files; do
     fi
 done
 
-.
+. $HOME/.bashrc
+
