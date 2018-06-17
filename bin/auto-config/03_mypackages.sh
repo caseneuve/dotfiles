2912e8110ee44254e08f62fee78735a03c39e8a3
caseneuve <piotr.kaznowski@gmail.com>
2018-06-17 02:29:29 +0200

    auto conf → my packages +

--- bin/auto-config/03_mypackages.sh
+++ bin/auto-config/03_mypackages.sh
@@ -2,11 +2,11 @@
 
 # Path:        ~/.dotfiles/bin/mypackages.sh
 # Created:     16.06.18, 15:15    @x200
-# Last update: 17.06.18, 02:24:45 @lenovo
+# Last update: 17.06.18, 02:29:24 @lenovo
 
 # Doc:
 
-packages="emacs tmux neomutt moc ranger i3-gaps i3blocks aspell aspell-pl r ditaa fzf docx2txt unrtf mpv gpick dropbox firefox dmenu rofi pulseaudio newsboat python-pip youtube-dl calcurse w3m compton transset-df gimp pacman-contrib"
+packages="emacs tmux neomutt moc ranger i3-gaps i3blocks aspell aspell-pl r ditaa fzf docx2txt unrtf mpv gpick dropbox firefox dmenu rofi pulseaudio newsboat python-pip youtube-dl calcurse w3m compton transset-df gimp pacman-contrib acpi"
 
 for x in $packages; do
     if pacman -Qs $x > /dev/null ; then
