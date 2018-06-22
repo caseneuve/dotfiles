0bed7731b0d0fb2e1e12e945638618423ef7fbcc
caseneuve <piotr.kaznowski@gmail.com>
2018-06-22 21:06:11 +0200

    tmux starter → start @ pane 1 (term)

--- bin/tmux-starter.sh
+++ bin/tmux-starter.sh
@@ -2,7 +2,7 @@
 
 # Path:        ~/.tmux/tmux-hg.sh
 # Created:     13.03.18, 10:30    @x200
-# Last update: 11.06.18, 07:17:12 @x200
+# Last update: 22.06.18, 21:05:37 @lenovo
 
 # Doc: starts tmux with 3 windows, the second one named "hg" with HANGUPS and the third "cal" with CALCURSE running
 # https://stackoverflow.com/questions/5609192/how-to-set-up-tmux-so-that-it-starts-up-with-specified-windows-opened
@@ -18,6 +18,6 @@ tmux select-window -t 'tmux:term'
 tmux splitw -h -p 30
 tmux clock-mode
 tmux select-pane -L
-tmux select-window -t 'tmux:cal'
+#tmux select-window -t 'tmux:cal'
 tmux -2 attach -d
 
