8af94e06865e59cf836b03da339ad522fbfa3ee7
caseneuve <piotr.kaznowski@gmail.com>
2018-07-03 03:35:58 +0200

    gitpull → colors

--- bin/gitpull.sh
+++ bin/gitpull.sh
@@ -2,35 +2,44 @@
 
 # ~/.dotfiles/bin/gitpull.sh
 # Created:     21.12.17           ?
-# Last update: 16.04.18, 16:23:24 @manjaroi3
+# Last update: 03.07.18, 03:35:07 @lenovo
 
 # Doc:
 #
 # Aktualizuje repozytoria 'dotfiles', 'emacs/load' oraz wspólny projekt z gitlaba  na lokalnym komputerze
 
-echo "Aktualizuję repo DOTFILES:"
-cd /home/piotr/.dotfiles 
-git pull
-echo -e "... ok!\n"
+# colors
+BLU="$(tput setab 4)"
+RED="$(tput setab 1)"
+RESET="$(tput sgr0)"
+BOLD="$(tput bold)"
+clear
 
-echo "Aktualizuję repo EMACS:"
+echo "${BLU} Aktualizuję repo ${RED}${BOLD} DOTFILES ${RESET}"
+cd /home/piotr/.dotfiles 
+echo -e "$(git pull)\n"
+
+echo "${BLU} Aktualizuję repo ${RED}${BOLD} EMACS ${RESET}"
 cd /home/piotr/.emacs.git 
-git pull
-echo -e "... ok!\n"
+echo -e "$(git pull)\n"
 
 if [ -d "/home/piotr/gitlab/liberal_artist_org" ]; then
-    echo "Aktualizuję repo LIBERAL ARTIST:"
+    echo "${BLU} Aktualizuję repo ${RED}${BOLD} LIBERAL ARTIST ${RESET}"
     cd /home/piotr/gitlab/liberal_artist_org 
-    git pull
-    echo -e "... ok!\n"
-fi
+    echo -e "$(git pull)\n"
+    fi
+
+if [ -d "/home/piotr/gitlab/py-exercises" ]; then
+    echo "${BLU} Aktualizuję repo ${RED}${BOLD} PY-EXERCISES ${RESET}"
+    cd /home/piotr/gitlab/py-exercises
+    echo -e "$(git pull)\n"
+    fi
 
 if [ -d "/home/piotr/suckless/st" ]; then
-    echo "Aktualizuję repo SUCKLESS TERMINAL:"
-    cd /home/piotr/suckless/st 
-    git pull
-    echo -e "... ok!\n"
-fi
+    echo "${BLU} Aktualizuję repo ${RED}${BOLD} SUCKLESS TERMINAL ${RESET}"
+    cd /home/piotr/suckless/st
+    echo -e "$(git pull)\n"
+    fi
 
 . ~/.bashrc
 
