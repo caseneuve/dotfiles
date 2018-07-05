ebed79756f8285a6a5940b5789dcf0b5f8ea77d8
caseneuve <piotr.kaznowski@gmail.com>
2018-07-05 14:33:53 +0200

    added signal=10 & pkill -TRMIN+10 i3blocks for: sys-upt,mailchecker

--- bin/neomutt-i3launcher.sh
+++ bin/neomutt-i3launcher.sh
@@ -2,9 +2,9 @@
 
 # Path:        ~/.dotfiles/bin/nmt-i3launcher.sh
 # Created:     20.05.18, 10:25    @lenovo
-# Last update: 20.05.18, 10:33:05 @lenovo
+# Last update: 05.07.18, 13:26:13 @toshiba
 
 # Doc: Open neomutt @ws 10, if already running, just go there
 
 [[ $(pidof neomutt) ]] && i3-msg -q 'workspace 10' || i3-msg -q 'workspace 10; exec --no-startup-id st -e neomutt'
-
+pkill -RTMIN+10 i3blocks
