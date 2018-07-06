e68ec4d4a36226e9de8dfd18355164cda22666a0
caseneuve <piotr.kaznowski@gmail.com>
2018-07-06 20:56:20 +0200

    sys upt → -RTMIN signal ok; BUGS?

--- bin/sys-updt.sh
+++ bin/sys-updt.sh
@@ -2,20 +2,23 @@
 
 # Path:        ~/.dotfiles/bin/sys-updt.sh
 # Created:     30.05.18, 16:02    @x200
-# Last update: 05.07.18, 15:09:20 @lenovo
+# Last update: 06.07.18, 20:50:47 @toshiba
 
-# Doc: Primitive pamac-tray replacement
-# NOTE: uses pacman-contrib package!
-# bug: ? spr. czy $DISTRO działa na wszystkich maszynach 
+# DOC: Primitive pamac-tray replacement
+# note: uses pacman-contrib package!
+# note: ? spr. czy $DISTRO działa na wszystkich maszynach 
 # note: zmieniam DISTRO na antergos (skoro wszędzie to samo)
+# BUG: ?? po kliknięciu (lewy lub prawy) i3blocks się zwiesza
+# TODO: dodać liczenie ile updatów do ikonki
 
 dir=~/.dotfiles/i3/bin
 check=`checkupdates`
 redbg=`tput setab 1`
 nor=`tput sgr0`
+DISTRO=antergos
 #DISTRO=`cat /etc/*-release | grep ^ID= | sed 's/^ID=\"\|\"\|ID=//g'`
 #DISTRO=`cat /etc/issue | awk '{print tolower($1)}' | head -n 1`
-DISTRO=antergos
+
 
 if ! [ -z "$check" ]; then
 #    echo -e "<span bgcolor=\"#e74c3c\"> ♻ </span>"
@@ -23,19 +26,18 @@ if ! [ -z "$check" ]; then
 fi
 
 case $BLOCK_BUTTON in
-    # left click = previous song
-    1) if ! [ -z "$check" ]; then
+    # left click 
+    1) if [ ! -z "$check" ]; then
            notify-send "System update:
---------------" "$check" -i $dir/$DISTRO-logo.png && exit 0;
+--------------" "$check" -i $dir/$DISTRO-logo.png; #&& exit 0;
        else
-           notify-send "No updates" -i $dir/$DISTRO-logo.png && exit 0;
+           notify-send "No updates" -i $dir/$DISTRO-logo.png; #&& exit 0;
        fi ;;
     # right click
-    3) if ! [ -z "$check" ]; then
-           i3-msg -q "exec --no-startup-id st -t sysupdt -e sudo pacman --noconfirm -Syyu" && pkill -RTMIN+12 i3blocks && exit 0
-           exit 0
-       else
-           exit 0
+    3) if [ ! -z "$check" ]; then
+           i3-msg -q "exec --no-startup-id st -t sysupdt -e sudo pacman --noconfirm -Syyu && pkill -RTMIN+12 i3blocks"; #&& exit 0
+       # else
+       #     exit 0
        fi;;
 esac
 
