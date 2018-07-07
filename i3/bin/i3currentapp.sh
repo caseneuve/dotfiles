18e5ef856b85774a891011b43159cde21ccbed47
caseneuve <piotr.kaznowski@gmail.com>
2018-07-07 12:39:14 +0200

    poprawiona składnia dla html (escape &)

--- i3/bin/i3currentapp.sh
+++ i3/bin/i3currentapp.sh
@@ -2,7 +2,7 @@
 
 # Path:        ~/.dotfiles/i3/bin/i3currentapp.sh
 # Created:     06.07.18, 20:24    @toshiba
-# Last update: 06.07.18, 22:05:14 @toshiba
+# Last update: 07.07.18, 12:34:04 @toshiba
 
 # Doc:
 
@@ -10,24 +10,27 @@ if [ "$(xrandr | grep -o 'VGA1 connected')" ]; then
     id=$(xprop -root | awk '/_NET_ACTIVE_WINDOW\(WINDOW\)/{print $NF}')
     app=$(xprop -id $id | grep WM_CLASS | awk '{print $4}' | sed 's/"//g' | tr '[:lower:]' '[:upper:]')
     name=$(xprop -id $id | awk '/_NET_WM_NAME/{$1=$2="";print}' | sed 's/"//g') # | cut -d'"' -f2)
-    colon=""
-
+    colon=" "
+    space=" "
+    
     namecolor=#008b8b
     appcolor=#ff4500
     # ff8c00
 
-    if [ "$app" = ""]; then
+    if [ "$app" = "" ]; then
         namecolor=""
         appcolor=""
+        space=""
+        colon=""
     elif [ "$app" = "QUTEBROWSER" ]; then
         app="QTB"
         colon=" "
         name=$(echo $name | sed 's/- qutebrowser//g')
-    elif [ "$app" == "MPV" ]; then
+    elif [ "$app" = "MPV" ]; then
         name=$(echo $name | sed 's/- mpv//g')
-    elif [ "$app" == "SLACK" ]; then
+    elif [ "$app" = "SLACK" ]; then
         name=$(echo $name | sed 's/Slack -//g')
-    elif [[ "$app" == "ST-256COLOR" && "$name" == "  dropdown" ]]; then
+    elif [[ "$app" = "ST-256COLOR" && "$name" == "  dropdown" ]]; then
         app="TMUX";
         colon=" "
         name="$(tmux list-windows | grep "\*" | awk '{print $2}' | sed 's/\*//')"
@@ -37,9 +40,10 @@ if [ "$(xrandr | grep -o 'VGA1 connected')" ]; then
         name=""
     elif [[ "$app" = "ST-256COLOR" ]]; then
         app="TERM";
-        colon=""    
     fi
+
+    name=$(echo $name | sed 's/&/&amp;/g')
     
-    echo "<span bgcolor=\"$appcolor\">$app</span><span bgcolor=\"$namecolor\">$colon$name</span>"
+    echo "<span bgcolor=\"$appcolor\">$space$app$space</span><span bgcolor=\"$namecolor\">$colon$name$colon</span>"
 fi
 
