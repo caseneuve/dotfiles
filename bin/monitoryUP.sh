f4ab66ae50d300eeb2b638fdd87f6917b74200f7
caseneuve <piotr.kaznowski@gmail.com>
2018-07-07 14:11:33 +0200

    poprawiony grep (xrandr "+" daje najlepszą resolution)

--- bin/monitoryUP.sh
+++ bin/monitoryUP.sh
@@ -2,14 +2,14 @@
 
 # Path:        ~/.dotfiles/bin/monitory-UP.sh
 # Created:     06.07.18, 09:47    @toshiba
-# Last update: 07.07.18, 12:21:00 @toshiba
+# Last update: 07.07.18, 14:05:29 @x200
 
 # Doc: default setting for 2 monitors (primary down, secondary up)
 
 intern=$(xrandr --listmonitors | awk 'NR==2 {print $(NF)}')
-inres=$(xrandr --current | grep "\*" | awk ' NR==1 {print $1}')
+inres=$(xrandr --current | grep "+$\|+ " | awk ' NR==1 {print $1}')
 extern=$(xrandr --listmonitors | awk 'NR==3 {print $(NF)}')
-exres=$(xrandr --current | grep "\*" | awk ' NR==2 {print $1}')
+exres=$(xrandr --current | grep "+$\|+ " | awk ' NR==2 {print $1}')
 posy=$(echo $exres | sed 's/[0-9]*x//')
 
 if xrandr | grep "$extern disconnected"; then
