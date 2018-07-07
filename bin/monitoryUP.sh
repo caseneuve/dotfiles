beea1ab27979da25666fd0cae560bdb61dd2ed21
caseneuve <piotr.kaznowski@gmail.com>
2018-07-07 12:52:48 +0200

    monitory up (updt)

--- bin/monitoryUP.sh
+++ bin/monitoryUP.sh
@@ -2,18 +2,20 @@
 
 # Path:        ~/.dotfiles/bin/monitory-UP.sh
 # Created:     06.07.18, 09:47    @toshiba
-# Last update: 06.07.18, 11:36:55 @toshiba
+# Last update: 07.07.18, 12:21:00 @toshiba
 
-# Doc:
+# Doc: default setting for 2 monitors (primary down, secondary up)
 
-intern=LVDS1
-extern=VGA1
+intern=$(xrandr --listmonitors | awk 'NR==2 {print $(NF)}')
+inres=$(xrandr --current | grep "\*" | awk ' NR==1 {print $1}')
+extern=$(xrandr --listmonitors | awk 'NR==3 {print $(NF)}')
+exres=$(xrandr --current | grep "\*" | awk ' NR==2 {print $1}')
+posy=$(echo $exres | sed 's/[0-9]*x//')
 
 if xrandr | grep "$extern disconnected"; then
     xrandr --output "$extern" --off --output "$intern" --auto
 else
-    xrandr --output LVDS1 --primary --mode 1366x768 --pos 0x1050 --rotate normal --output "$extern" --mode 1680x1050 --pos 0x0 --rotate normal
+    xrandr --output "$intern" --primary --mode "$inres" --pos 0x$posy --rotate normal --output "$extern" --mode "$exres" --pos 0x0 --rotate normal
+    ~/.fehbg
 fi
-
-~/.fehbg
 
