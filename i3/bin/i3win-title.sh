9e04d038adcbac8ea4cb494d4bf761ac43ff4269
caseneuve <piotr.kaznowski@gmail.com>
2018-07-11 00:52:39 +0200

    i3win-title: chyba popr. copy to xclip

--- i3/bin/i3win-title.sh
+++ i3/bin/i3win-title.sh
@@ -2,13 +2,15 @@
 
 # Path:        ~/.dotfiles/i3/bin/i3win-title.sh
 # Created:     10.07.18, 21:21    @x200
-# Last update: 11.07.18, 00:07:01 @lenovo
+# Last update: 11.07.18, 00:46:23 @x200
 
 # Doc:
 
 name=`xdotool getactivewindow getwindowname`
 len=`echo ${#name}`
 max=40
+
+name=`echo $name | sed 's/ - qutebrowser$\|mpv//g'`
 
 if (( $len > 0 )); then
     if (( $len > $max )); then
@@ -20,4 +22,10 @@ if (( $len > 0 )); then
 else
     echo 
 fi
+
+case $BLOCK_BUTTON in
+    # left click 
+    1) notify-send "Focused window title:
+---------------------" "$name\ncopied to clipboard" && i3-msg "exec --no-startup-id echo $name | xclip -selection c && exit 1";;
+esac
 
