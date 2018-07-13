658fbe3b1b7a5999439592c2da2d64a579777d4b
caseneuve <piotr.kaznowski@gmail.com>
2018-07-13 12:52:58 +0200

    git pull/stats colors popr.

--- bin/gitpull.sh
+++ bin/gitpull.sh
@@ -2,52 +2,52 @@
 
 # ~/.dotfiles/bin/gitpull.sh
 # Created:     21.12.17           ?
-# Last update: 03.07.18, 17:14:59 @x200
+# Last update: 13.07.18, 12:44:45 @lenovo
 
 # Doc:
 # Aktualizuje repozytoria 'dotfiles', 'emacs/load' oraz wspólny projekt z gitlaba  na lokalnym komputerze
 # TODO: loop?
 
 # colors
-BLU="$(tput setab 4)"
-RED="$(tput setab 1)"
+ORANGE="$(tput setaf 11)"
+RED="$(tput setaf 9)"
 RESET="$(tput sgr0)"
 BOLD="$(tput bold)"
 clear
 
-echo "${BLU} Aktualizuję repo ${RED}${BOLD} DOTFILES ${RESET}"
+echo "${BOLD}${ORANGE}Aktualizuję repo ${RED}DOTFILES ${RESET}"
 cd /home/piotr/.dotfiles 
 git pull 
 echo
 
-echo "${BLU} Aktualizuję repo ${RED}${BOLD} EMACS ${RESET}"
+echo "${BOLD}${ORANGE}Aktualizuję repo ${RED}EMACS ${RESET}"
 cd /home/piotr/.emacs.git 
 git pull 
 echo
 
 if [ -d "/home/piotr/gitlab/liberal_artist_org" ]; then
-    echo "${BLU} Aktualizuję repo ${RED}${BOLD} LIBERAL ARTIST ${RESET}"
+    echo "{BOLD}${ORANGE}Aktualizuję repo ${RED}LIBERAL ARTIST ${RESET}"
     cd /home/piotr/gitlab/liberal_artist_org 
     git pull
     echo
     fi
 
 if [ -d "/home/piotr/gitlab/py-exercises" ]; then
-    echo "${BLU} Aktualizuję repo ${RED}${BOLD} PY-EXERCISES ${RESET}"
+    echo "${BOLD}${ORANGE}Aktualizuję repo ${RED}PY-EXERCISES ${RESET}"
     cd /home/piotr/gitlab/py-exercises
     git pull
     echo
     fi
 
 if [ -d "/home/piotr/suckless/st" ]; then
-    echo "${BLU} Aktualizuję repo ${RED}${BOLD} SUCKLESS TERMINAL ${RESET}"
+    echo "${BOLD}${ORANGE}Aktualizuję repo ${RED}SUCKLESS TERMINAL ${RESET}"
     cd /home/piotr/suckless/st
     git pull
     echo
     fi
 
 if [ -d "/home/piotr/github/emacs-htmlize" ]; then
-    echo "${BLU} Aktualizuję repo ${RED}${BOLD} EMACS: htmlize ${RESET}"
+    echo "${BOLD}${ORANGE}Aktualizuję repo ${RED}EMACS: htmlize ${RESET}"
     cd /home/piotr/github/emacs-htmlize
     git pull
     echo
--- bin/gitstatus.sh
+++ bin/gitstatus.sh
@@ -2,70 +2,70 @@
 
 # ~/.dotfiles/bin/gitstatus.sh
 # Created:     27.12.17           ?
-# Last update: 03.07.18, 03:45:16 @lenovo
+# Last update: 13.07.18, 12:51:52 @lenovo
 
 # Doc:
 # Podaje status repozytoriów 'dotfiles' i 'emacs' na lokalnym komputerze
 
 # colors
-YELb=$(tput setab 3)
-REDb=$(tput setab 1)
-CYAN=$(tput setab 6)
+YEL=$(tput setaf 3)
+RED=$(tput setaf 9)
+GREEN=$(tput setaf 10)
 RESET=$(tput sgr0)
 BOLD=$(tput bold)
 
 clear
 
 # .dotfiles
-echo "${CYAN} Sprawdzam status repo ${RESET}${BOLD}${REDb} DOTFILES ${RESET}"
+echo "${BOLD}${GREEN}Sprawdzam status repo ${RED}DOTFILES ${RESET}"
 cd /home/piotr/.dotfiles/
 if [[ `git status --porcelain` ]]; then
-    echo -e "${YELb}${BOLD} >>$(git status --porcelain) ${RESET}\n"
+    echo -e "${YEL}${BOLD}$(git status --porcelain) ${RESET}\n"
 else
-    echo -e " $(git status | sed -n '2p')\n"
+    echo -e "$(git status | sed -n '2p')\n"
 fi
 
 # .emacs.git
-echo "${CYAN} Sprawdzam status repo ${RESET}${BOLD}${REDb} EMACS ${RESET}"
+echo "${BOLD}${GREEN}Sprawdzam status repo ${RED}EMACS ${RESET}"
 cd /home/piotr/.emacs.git
 if [[ `git status --porcelain` ]]; then
-    echo -e "${YELb}${BOLD} >>$(git status --porcelain) ${RESET}\n"
+    echo -e "${YEL}${BOLD}>>$(git status --porcelain) ${RESET}\n"
 else
-    echo -e " $(git status | sed -n '2p')\n"
+    echo -e "$(git status | sed -n '2p')\n"
 fi
 
 # liberal artist
 if [ -d "/home/piotr/gitlab/liberal_artist_org" ]; then
-    echo "${CYAN} Sprawdzam status repo ${RESET}${BOLD}${REDb} LIBERAL ARTIST: ${RESET}" 
+    echo "${BOLD}${GREEN}Sprawdzam status repo ${RED}LIBERAL ARTIST: ${RESET}" 
     cd /home/piotr/gitlab/liberal_artist_org
     if [[ `git status --porcelain` ]]; then
-        echo -e "${YELb}${BOLD} >>$(git status --porcelain) ${RESET}\n"
+        echo -e "${YEL}${BOLD}>>$(git status --porcelain) ${RESET}\n"
     else
-    echo -e " $(git status | sed -n '2p')\n"
+    echo -e "$(git status | sed -n '2p')\n"
     fi
 fi
 
 # py exercises
 if [ -d "/home/piotr/gitlab/py-exercises" ]; then
-    echo "${CYAN} Sprawdzam status repo ${RESET}${BOLD}${REDb} PY-EXERCISES ${RESET}" 
+    echo "${BOLD}${GREEN}Sprawdzam status repo ${RED}PY-EXERCISES ${RESET}" 
     cd /home/piotr/gitlab/py-exercises
     if [[ `git status --porcelain` ]]; then
-        echo -e "${YELb}${BOLD} >>$(git status --porcelain) ${RESET}\n"
+        echo -e "${YEL}${BOLD}>>$(git status --porcelain) ${RESET}\n"
     else
-        echo -e " $(git status | sed -n '2p')\n"
+        echo -e "$(git status | sed -n '2p')\n"
     fi
 fi
 
 # repo st 
 if [ -d "/home/piotr/suckless/st" ]; then
-    echo "${CYAN} Sprawdzam status repo ${RESET}${BOLD}${REDb} SUCKLESS TERMINAL ${RESET}"
+    echo "${BOLD}${GREEN}Sprawdzam status repo ${RED}SUCKLESS TERMINAL ${RESET}"
     cd /home/piotr/suckless/st
     if [[ `git status --porcelain` ]]; then
-        echo -e "${YELb}${BOLD} >>$(git status --porcelain) ${RESET}\n"
+        echo -e "${YEL}${BOLD}>>$(git status --porcelain) ${RESET}\n"
     else
-        echo -e " $(git status | sed -n '2p')\n"
+        echo -e "$(git status | sed -n '2p')\n"
     fi
 fi
 
-echo -e " ...zrobione!\n"
+#echo -e "...zrobione!\n"
 
