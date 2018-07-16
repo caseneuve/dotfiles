459e4b095078d17fbe25888ed72dc95e902e15cf
caseneuve <piotr.kaznowski@gmail.com>
2018-07-17 00:45:24 +0200

    ujednolicić komendy (czy można łączyć mpv audio i video?)

--- bin/urlview-handler.sh
+++ bin/urlview-handler.sh
@@ -2,10 +2,11 @@
 
 # Path:        ~/.bin/urlview-handler.sh
 # Created:     04.04.18, 11:48    @x200
-# Last update: 13.07.18, 10:32:04 @toshiba
+# Last update: 17.07.18, 00:40:56 @lenovo
 
 # Doc: via Luke Smith github
 # note: replacing feh with sxiv
+# done: problem na współpracy z queuemv → argument sie nie rozwija, lecz cytuje się nazwa (należy cytuwać podwójnymi nawiasami)
 
 ext="${1##*.}"
 mpvFiles="mkv mp4 avi mov wmv flv"
@@ -21,10 +22,15 @@ elif echo $mpvFiles | grep -w $ext > /dev/null; then
     [[ -f $mpvscript ]] && nohup $mpvscript "$1" >/dev/null || nohup mpv --loop --quiet "$1" > /dev/null &
 elif echo $wgetFiles | grep -w $ext > /dev/null; then
     nohup wget "$1" >/dev/null &
-elif echo $1 | grep "youtube\." > /dev/null; then
-    [[ -f $mpvscript ]] && nohup ~/bin/queuempv "$1" >/dev/null && notify-send -u low "Urlview" "Video:\n<i>`curl -s $1 | grep -o \"<title>[^<]*\" | tail -c+8`</i>\nis loading." || nohup mpv "$1" > /dev/null &
-elif echo $1 | grep youtu.be > /dev/null; then
-    nohup mpv "$1" > /dev/null &
+elif echo $1 | grep "youtube\.\|youtu\.be" > /dev/null; then
+    if [[ -f $mpvscript ]]; then
+        nohup $mpvscript "$1" &
+        notify-send -u low "Urlview" "Video:\n\n<i>`curl -s $1 | grep -o \"<title>[^<]*\" | tail -c+8`</i>\n\nis loading."
+    else
+        nohup mpv "$1" > /dev/null &
+    fi
+# elif echo $1 | grep youtu.be > /dev/null; then
+#      nohup mpv "$1" > /dev/null &
 elif echo $1 | grep vimeo > /dev/null; then
     nohup mpv "$1" > /dev/null &
 else
