cd65b70c563d4c9eddcce2b2d656d9cf10c6badd
caseneuve <piotr.kaznowski@gmail.com>
2018-07-16 19:09:33 +0200

    poprawione wychwytywanie $extern

--- bin/monitoryUP.sh
+++ bin/monitoryUP.sh
@@ -2,14 +2,14 @@
 
 # Path:        ~/.dotfiles/bin/monitory-UP.sh
 # Created:     06.07.18, 09:47    @toshiba
-# Last update: 12.07.18, 17:26:47 @x200
+# Last update: 16.07.18, 19:05:02 @x200
 
 # Doc: default setting for 2 monitors (primary down, secondary up)
 #xrandr --current | grep -A 1 primary | awk -F"x" 'NR==2 {gsub(/^ */,""); print $1}'
 intern=$(xrandr --listmonitors | awk 'NR==2 {print $(NF)}')
 #intern=$(xrandr --current | awk 'NR==2 {print $1}')
 inres=$(xrandr --current | grep "+$\|+ " | awk ' NR==1 {print $1}')
-extern=$(xrandr --listmonitors | awk 'NR==3 {print $(NF)}')
+extern=$(xrandr | grep " connected" | awk 'NR==2 {print $1}')
 #extern=$(xrandr --current | grep -v $intern | grep -A 1 " connected" | awk '{print $1}')
 exres=$(xrandr --current | grep "+$\|+ " | awk ' NR==2 {print $1}')
 posy=$(echo $exres | sed 's/[0-9]*x//')
