dd1570032acecb1f4957214d219ff682b9469c5b
caseneuve <piotr.kaznowski@gmail.com>
2018-08-04 10:31:37 +0200

    korekta

--- bin/screencast.sh
+++ bin/screencast.sh
@@ -1,21 +1,22 @@
 #!/bin/bash
 
-if [ ! -d "$HOME/Wideo/cast" ]; then
-    mkdir $HOME/Wideo/cast
-fi;
+# Last update: 03.08.18, 10:47:18 @x200
 
-pkill compton;
+CASTDIR=$HOME/wid/cast
+[[ -d $CASTDIR ]] || mkdir -p $CASTDIR
 
 if [ "$1" = '--help' ];then
-    echo 'Usage ./screencast.sh [filename]'
+    echo 'usage: ~/dotfiles/bin/screencast.sh [filename]'
 else
-    dimensions=`xdpyinfo | grep 'dimensions:'|awk '{print $2}'`
-    if [ -n "$1" ];then
-        outFile="$HOME/Wideo/cast/$1.mkv"
+    dimensions=$(xdpyinfo | grep 'dimensions:' | awk '{print $2}')
+    if [ -n "$1" ]; then
+        outFile="$CASTDIR/$1.mkv"
     else
-        outFile="$HOME/Wideo/cast/out.mkv"
+        outFile="$CASTDIR/out.mkv"
     fi
+#    pkill compton;
     ffmpeg -f x11grab -s "$dimensions" -i :0.0 "$outFile"
 fi
 
-compton
+[[ $(ps aux | grep compton | grep -v grep) ]] || compton &
+
