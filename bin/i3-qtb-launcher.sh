c004cea1c258140e2dbab5ea0393a3b8d06f0a78
caseneuve <piotr.kaznowski@gmail.com>
2018-08-15 00:55:41 +0200

    todo: poprawić na podobieństwo i3-emacs-launcher

--- bin/i3-qtb-launcher.sh
+++ bin/i3-qtb-launcher.sh
@@ -2,10 +2,11 @@
 
 # Path:        ~/.dotfiles/bin/qtb-launcher.sh
 # Created:     26.05.18, 23:42    @x200
-# Last update: 13.08.18, 15:53:38 @x200
+# Last update: 15.08.18, 00:50:00 @x200
 
 # Doc: If qutebrowser is running goes to the first workspace with qtb window; if not -- launches qtb at wksp 2
-# TODO: przepisać to na py (i3ipc)
+# TODO: przepisać to na py (i3ipc) albo użyć wmctrl
+
 ws=$(i3-msg -t get_tree | grep -oE "num\":[0-9]*||qutebrowser" | tr "\n" " " | grep -o "num\":[0-9]* qutebrowser" | grep -o [0-9]* | head -n 1)
 
 [[ "$ws" ]] && i3-msg "workspace $ws" || i3-msg "workspace 2; exec --no-startup-id qutebrowser"
