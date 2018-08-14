268da610fa61de763a865b05eb5bffc75367ba09
caseneuve <piotr.kaznowski@gmail.com>
2018-08-15 00:55:24 +0200

    poprawki z wykorzystaniem wmctrl

--- bin/i3-emacs-launcher.sh
+++ bin/i3-emacs-launcher.sh
@@ -2,12 +2,34 @@
 
 # Path:        ~/.dotfiles/bin/i3-emacs-launcher.sh
 # Created:     27.05.18, 09:16    @x200
-# Last update: 13.08.18, 15:54:38 @x200
+# Last update: 15.08.18, 00:28:20 @x200
 
-# Doc: If emacs is running goes to the first workspace with qtb window; if not -- launches qtb at wksp 1
+# >> DOC:
+# "If emacs is running goes to the first workspace with qtb window; if not -- launches qtb at wksp 1"
+# note: requires `wmctrl` package (pacman)
+
+# >> TODOS:
+# todo: dodać opcje dla większej liczby otwartych okien (niech cyklicznie przechodzi po następnych)
 # todo: przepisać to na py (i3ipc)
 
-ws=$(i3-msg -t get_tree | grep -oE "num\":[0-9]*||emacs" | tr "\n" " " | grep -o "num\":[0-9]* emacs" | grep -o [0-9]* | head -n 1)
+# >> VARIABLES:
+EMACS_GUI=$(wmctrl -lx | grep "emacs.Emacs" | grep -v grep)
+[[ $EMACS_GUI ]] && EMACS_GUI_WS=$(echo $EMACS_GUI | awk '{print $2}')
+FOCUSED_WS=$(wmctrl -d | grep "*" | awk '{print $1}')
+EMACS_CLI=$(wmctrl -lx | grep "emacsclient" | grep -v grep)
 
-[[ "$ws" ]] && i3-msg "workspace $ws" || i3-msg "workspace 1; exec --no-startup-id emacs"
+# >> RETURN:
+if [[ $EMACS_GUI ]]; then
+    if [[ $FOCUSED_WS == $EMACS_GUI_WS ]]; then
+        [[ $EMACS_CLI ]] && wmctrl -a emacsclient || notify-send -u low "i3" "<i>No other instances of Emacs visible</i>"
+    else
+        wmctrl -a Emacs
+    fi
+else
+    echo 'i3-msg "workspace 1; exec --no-startup-id emacs"'
+fi
+
+# >> SPADY:
+# ws=$(i3-msg -t get_tree | grep -oE "num\":[0-9]*||emacs" | tr "\n" " " | grep -o "num\":[0-9]* emacs" | grep -o [0-9]* | head -n 1)
+# [[ "$ws" ]] && i3-msg "workspace $ws" || i3-msg "workspace 1; exec --no-startup-id emacs"
 
--- bin/neomutt-i3launcher.sh
+++ bin/neomutt-i3launcher.sh
@@ -2,9 +2,23 @@
 
 # Path:        ~/.dotfiles/bin/nmt-i3launcher.sh
 # Created:     20.05.18, 10:25    @lenovo
-# Last update: 05.07.18, 13:26:13 @toshiba
+# Last update: 15.08.18, 00:47:26 @x200
 
-# Doc: Open neomutt @ws 10, if already running, just go there
+# >> DOC:
+# "Open neomutt @ws 10, if already running, just go there"
 
-[[ $(pidof neomutt) ]] && i3-msg -q 'workspace 10' || i3-msg -q 'workspace 10; exec --no-startup-id st -e neomutt'
+# >> TODOS:
+# todo: zastąpić st-256clor zmienną dla terinala
+# done: niech zawsze fokusuje się na neomutcie, gdy wchodzi do ws10 (alt: i3ipc/xdotool)
+
+NEOMUTT_ID=$(wmctrl -lx | grep "st-256color.st-256color" | grep neomutt | grep -v grep | awk '{print $1}')
+
+if [[ $(pidof neomutt) ]]; then
+    #i3-msg -q 'workspace 10'
+    wmctrl -ia $NEOMUTT_ID
+else
+    i3-msg -q 'workspace 10; exec --no-startup-id st -e neomutt'
+fi
+
 pkill -RTMIN+10 i3blocks
+
