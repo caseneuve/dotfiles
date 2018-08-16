37e026cc22ecb3741efbe73b2e18548d1f12d29d
caseneuve <piotr.kaznowski@gmail.com>
2018-08-16 21:04:16 +0200

    zmienna wywalała komendę i3msg (go verbose again)

--- bin/i3-newsboat-launcher.sh
+++ bin/i3-newsboat-launcher.sh
@@ -2,21 +2,20 @@
 
 # Path:        ~/.dotfiles/bin/i3-newsboat-launcher.sh
 # Created:     16.08.18, 19:08    @x200
-# Last update: 16.08.18, 19:26:10 @x200
+# Last update: 16.08.18, 20:32:36 @x200
 
 # >> DOC:
 # note: requires `wmctrl`
 # "focus newsboat or start one"
 
 # >> VARIABLES:
-I3_NAME=newsboat
 NEWSBOAT_ID=$(wmctrl -lx | grep "st-256color.st-256color" | awk '{print $5 " " $1}' | grep newsboat | awk '{print $2}')
 
 # >> RUN: 
-if [[ $(pidof $I3_NAME) ]]; then
+if [[ $(pidof newsboat) ]]; then
     #i3-msg -q 'workspace 10'
     wmctrl -ia $NEWSBOAT_ID
 else
-    i3-msg -q 'workspace 10; exec --no-startup-id st -e $I3_NAME'
+    i3-msg -q 'workspace 10; exec --no-startup-id st -e newsboat'
 fi
 
