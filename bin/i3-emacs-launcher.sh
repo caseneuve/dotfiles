5e6089af0aab42a8fc74cd28eb5ffa11899538ac
caseneuve <piotr.kaznowski@gmail.com>
2018-08-20 02:33:39 +0200

    bugs: todo fixme (nie togluje, nie chwyta wszystkich inst. if > 2)

--- bin/i3-emacs-launcher.sh
+++ bin/i3-emacs-launcher.sh
@@ -2,13 +2,14 @@
 
 # Path:        ~/.dotfiles/bin/i3-emacs-launcher.sh
 # Created:     27.05.18, 09:16    @x200
-# Last update: 19.08.18, 02:53:18 @x200
+# Last update: 20.08.18, 02:29:15 @lenovo
 
 # >> DOC:
 # "If emacs is running goes to the first workspace with qtb window; if not -- launches qtb at wksp 1"
 # note: requires `wmctrl` package (pacman)
 
 # >> TODOS:
+# fixme: gdy 2 instancje E. na 1 ws, nie togluje (spr mutt)
 # todo: dodać opcje dla większej liczby otwartych okien (niech cyklicznie przechodzi po następnych)
 # todo: przepisać to na py (i3ipc)
 
@@ -22,12 +23,12 @@ EMACS_GUI_ID=$(wmctrl -lx | awk '/emacs.Emacs/ && !/st-256color/ {print $1}')
 
 [[ $EMACS_GUI ]] && EMACS_GUI_WS=$(echo $EMACS_GUI | awk '{print $2}')
 FOCUSED_WS=$(wmctrl -d | awk '/*/ {print $1}')
-EMACS_CLI=$(wmctrl -lx | grep "emacsclient" | grep -v grep)
+EMACS_CLI=$(wmctrl -lx | grep "emacsclient\| e " | grep -v grep | awk '{print $1}')
 
 # >> RETURN:
 if [[ $EMACS_GUI ]]; then
     if [[ $FOCUSED_WS == $EMACS_GUI_WS ]]; then
-        [[ $EMACS_CLI ]] && wmctrl -a emacsclient || notify-send -u low "i3" "<i>No other instances of Emacs visible</i>"
+        [[ $EMACS_CLI ]] && wmctrl -ia $EMACS_CLI || notify-send -u low "i3" "<i>No other instances of Emacs visible</i>"
     else
         wmctrl -ia $EMACS_GUI_ID
     fi
