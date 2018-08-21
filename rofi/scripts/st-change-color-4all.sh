772b69d2b0f04fa3baf1e15e7c0fe5fba89e81c4
caseneuve <piotr.kaznowski@gmail.com>
2018-08-21 14:08:17 +0200

    poprawione wyszukiwanie

--- rofi/scripts/st-change-color-4all.sh
+++ rofi/scripts/st-change-color-4all.sh
@@ -2,13 +2,15 @@
 
 # Path:        ~/bin/st-change-color-4all.sh
 # Created:     13.07.18, 15:38    @lenovo
-# Last update: 13.07.18, 16:11:24 @lenovo
+# Last update: 21.08.18, 14:07:51 @x200
 
 # Doc: change theme in all st instances simultaneously
 
-windows=$(xdotool search --class st)
+ST_WIND=$(xdotool search --class st)
+SCRATCH_WIND=$(xdotool search --class scratch)
+WINDOWS="$ST_WIND $SCRATCH_WIND"
 
-for i in $windows; do
+for i in $WINDOWS; do
     xdotool key --window $i ctrl+F6
 done
 
