7c49385504fb1c5ce66228324ddf62020aab5259
caseneuve <piotr.kaznowski@gmail.com>
2018-08-24 17:40:16 +0200

    title → info bout files

--- bin/sendmailquick.sh
+++ bin/sendmailquick.sh
@@ -9,6 +9,7 @@ red=$(tput setaf 1)
 blu=$(tput setaf 4)
 whibg=$(tput setab 15)
 blafg=$(tput setaf 0)
+[[ ${#@} > 1 ]] && ATT=" (& $((${#@} - 1)) more...)" || ATT=
 
 adbook='/home/piotr/box/Dropbox/.Config/Mutt/.mutt_aliases'
 MAILDIR='/home/piotr/box/Dropbox/.Config/Mutt/.sent'
@@ -36,8 +37,8 @@ if [ -z "$1" ]; then
     echo "${red}${bold}Nie zaznaczyłeś załączników!${normal}";
 else
     echo -e "Quickmail sent via RANGER, $(date '+%d/%m/%Y, %H:%M:%S')\n-----------------------------------------------\nDo:     $mailddrs\nAtt:    $@\n" > $MAILDIR/ranger_quicksent-$(date +%y%m%dT%H%M)
-    echo -e "Wiadomość wysłana automatycznie $(date '+%d/%m o %H:%M'), zawiera następujące pliki\n<< $@ >> \n\n" | neomutt -s "ranger" $mailddrs -a "$@" > /dev/null 2>&1 &&\
-        echo -e "\n${gre}Wysłałem wiadomość do ${normal}${bold}$mailddrs${normal}${gre} z następującymi załącznikami:\n${whibg}${blafg}${bold}'$@'${normal}\n"
+    echo -e "Wiadomość wysłana automatycznie $(date '+%d/%m o %H:%M'), zawiera następujące pliki:\n$(echo $@ | tr ' ' '\n')\n\n" | neomutt -s "[rng] $1$ATT" $mailddrs -a "$@" > /dev/null 2>&1 &&\
+        echo -e "\n${gre}Wysłałem wiadomość do ${normal}${bold}$mailddrs${normal}${gre} z następującymi załącznikami:\n${whibg}${blafg}${bold}$(echo $@ | tr ' ' '\n')${normal}\n"
 fi
 
 sleep 3s
