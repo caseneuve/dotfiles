33e790f6d1cd31e4490a5fbe68ff1446a334e3fc
caseneuve <piotr.kaznowski@gmail.com>
2018-08-25 19:30:14 +0200

    poprawiony → loop & warunki; kolory b/w

--- bin/gitpull.sh
+++ bin/gitpull.sh
@@ -2,7 +2,7 @@
 
 # ~/.dotfiles/bin/gitpull.sh
 # Created:     21.12.17           ?
-# Last update: 25.08.18, 19:22:28 @toshiba
+# Last update: 25.08.18, 19:29:37 @toshiba
 
 # Doc:
 # Aktualizuje repozytoria 'dotfiles', 'emacs/load' oraz wspólny projekt z gitlaba  na lokalnym komputerze
@@ -16,50 +16,24 @@ BOLD="$(tput bold)"
 
 DOTFILES=/home/piotr/.dotfiles
 EMACS=/home/piotr/.emacs.git
-#ARTIST=/home/piotr/git/lab/liberal_artist_org
 PY_EX=/home/piotr/git/lab/py-exercises
 ST=/home/piotr/git/hub/st
 EMACS_HTMLIZE=/home/piotr/git/hub/emacs-htmlize
+#ARTIST=/home/piotr/git/lab/liberal_artist_org
 
 clear
 
-echo "${BOLD}${ORANGE}Aktualizuję repo ${RED}DOTFILES ${RESET}"
-cd $DOTFILES
-git pull 
-echo
-
-echo "${BOLD}${ORANGE}Aktualizuję repo ${RED}EMACS ${RESET}"
-cd $EMACS 
-git pull 
-echo
-
-# if [ -d $ARTIST ]; then
-#     echo "${BOLD}${ORANGE}Aktualizuję repo ${RED}LIBERAL ARTIST ${RESET}"
-#     cd $ARTIST 
-#     git pull
-#     echo
-# fi
-
-if [ -d $PY_EX ]; then
-    echo "${BOLD}${ORANGE}Aktualizuję repo ${RED}PY-EXERCISES ${RESET}"
-    cd $PY_EX
-    git pull
-    echo
-fi
-
-if [ -d $ST ]; then
-    echo "${BOLD}${ORANGE}Aktualizuję repo ${RED}SUCKLESS TERMINAL ${RESET}"
-    cd $ST
-    git pull
-    echo
-fi
-
-if [ -d $EMACS_HTMLIZE ]; then
-    echo "${BOLD}${ORANGE}Aktualizuję repo ${RED}EMACS: htmlize ${RESET}"
-    cd $EMACS_HTMLIZE
-    git pull
-    echo
-fi
+for i in $DOTFILES $EMACS $PY_EX $ST $EMACS_HTMLIZE
+do
+    if [[ -d $i ]]; then
+        echo "## Aktualizuję repo $i"
+        cd $i
+        git pull
+        echo
+    else
+        echo -e "!! Nie ma takiego repo:\n$i"
+    fi
+done
 
 . ~/.bashrc
 
