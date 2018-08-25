c7d6d88ecf022ed799d173605971306f7f9e2e78
caseneuve <piotr.kaznowski@gmail.com>
2018-08-25 19:33:48 +0200

    bold added

--- bin/gitpull.sh
+++ bin/gitpull.sh
@@ -2,15 +2,15 @@
 
 # ~/.dotfiles/bin/gitpull.sh
 # Created:     21.12.17           ?
-# Last update: 25.08.18, 19:29:37 @toshiba
+# Last update: 25.08.18, 19:33:41 @toshiba
 
 # Doc:
 # Aktualizuje repozytoria 'dotfiles', 'emacs/load' oraz wspólny projekt z gitlaba  na lokalnym komputerze
 # TODO: loop?
 
 # colors
-ORANGE="$(tput setaf 11)"
-RED="$(tput setaf 9)"
+# ORANGE="$(tput setaf 11)"
+# RED="$(tput setaf 9)"
 RESET="$(tput sgr0)"
 BOLD="$(tput bold)"
 
@@ -26,7 +26,7 @@ clear
 for i in $DOTFILES $EMACS $PY_EX $ST $EMACS_HTMLIZE
 do
     if [[ -d $i ]]; then
-        echo "## Aktualizuję repo $i"
+        echo "## Aktualizuję repo $BOLD$(basename $i)$RESET:"
         cd $i
         git pull
         echo
