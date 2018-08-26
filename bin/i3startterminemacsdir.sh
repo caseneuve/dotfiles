7a001a1667d80321fda0c9969a024ceaa4eff6f3
caseneuve <piotr.kaznowski@gmail.com>
2018-08-26 16:54:30 +0200

    check if DIR exists (emacs buffers case)

--- bin/i3startterminemacsdir.sh
+++ bin/i3startterminemacsdir.sh
@@ -2,7 +2,7 @@
 
 # Path:        ~/biu/bash/cdemacsdir.sh
 # Created:     26.08.18, 15:00    @x200
-# Last update: 26.08.18, 16:09:51 @x200
+# Last update: 26.08.18, 16:53:48 @x200
 
 # >> DOC:
 # If an Emacs window is focused, open the terminal in the working directory, 
@@ -15,7 +15,7 @@ DIR=${FULL%$FILE}
 FOC=$(~/bin/i3move -i | awk '/Class:/ {print $2}')
 
 # >> RUN:
-if [[ $FOC == 'Emacs' ]]; then
+if [[ $FOC == 'Emacs' && -d $DIR ]]; then
     cd $DIR && st -t "st @$(date +%H:%M:%S)" &
 else
     st -t "st @$(date +%H:%M:%S)" &
