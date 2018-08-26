773eb9ba0621bdb2f09d97d89f7a91772bbef26b
caseneuve <piotr.kaznowski@gmail.com>
2018-08-26 21:20:12 +0200

    docs added; i3move → i3get

--- bin/i3startterminemacsdir.sh
+++ bin/i3startterminemacsdir.sh
@@ -2,9 +2,13 @@
 
 # Path:        ~/biu/bash/cdemacsdir.sh
 # Created:     26.08.18, 15:00    @x200
-# Last update: 26.08.18, 16:56:56 @x200
+# Last update: 26.08.18, 21:19:57 @x200
 
 # >> DOC:
+# Requires:
+# - i3get.py → git: py-exercises (26/08/2018)
+# - i3ipc as dep.
+# - wmctrl
 # If an Emacs window is focused with a file in active buffer,
 # open the terminal in the working directory; 
 # else, open in $HOME.
@@ -13,7 +17,7 @@
 FULL=$(wmctrl -lx | awk '/emacs.Emacs/ {print $5}')
 FILE=$(basename "$FULL")
 DIR=${FULL%$FILE}
-FOC=$(~/bin/i3move -i | awk '/Class:/ {print $2}')
+FOC=$(~/bin/i3get -c)
 
 # >> RUN:
 if [[ $FOC == 'Emacs' && -d $DIR ]]; then
