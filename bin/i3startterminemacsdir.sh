00036d7cdc97e0ae96ff3a304a4c82b4961054ff
caseneuve <piotr.kaznowski@gmail.com>
2018-08-26 16:57:01 +0200

    docs added

--- bin/i3startterminemacsdir.sh
+++ bin/i3startterminemacsdir.sh
@@ -2,11 +2,12 @@
 
 # Path:        ~/biu/bash/cdemacsdir.sh
 # Created:     26.08.18, 15:00    @x200
-# Last update: 26.08.18, 16:53:48 @x200
+# Last update: 26.08.18, 16:56:56 @x200
 
 # >> DOC:
-# If an Emacs window is focused, open the terminal in the working directory, 
-# else, open in $HOME
+# If an Emacs window is focused with a file in active buffer,
+# open the terminal in the working directory; 
+# else, open in $HOME.
 
 # >> VARIABLES:
 FULL=$(wmctrl -lx | awk '/emacs.Emacs/ {print $5}')
