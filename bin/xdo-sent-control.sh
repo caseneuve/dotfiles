2169f2ca9427a12035bdb6fb29010923f41639a6
caseneuve <piotr.kaznowski@gmail.com>
2018-08-29 00:02:53 +0200

    clear

--- bin/xdo-sent-control.sh
+++ bin/xdo-sent-control.sh
@@ -2,7 +2,7 @@
 
 # Path:        ~/.dotfiles/bin/xdo-sent-control.sh
 # Created:     28.08.18, 12:40    @x200
-# Last update: 28.08.18, 23:51:31 @x200
+# Last update: 29.08.18, 00:02:48 @x200
 
 # >> DOC:
 # Dep: xdotool
@@ -27,15 +27,3 @@ while getopts 'agnprq' flag; do
     fi
 done
 
-# if [[ -n $SENT_ID ]]; then
-#     for i in $SENT_ID; do
-#         while getopts 'agnprq' flag; do
-#             case "${flag}" in
-#                 a|g|n|p|r|q) xdotool key --window $i $flag 2>/dev/null
-#                              ;;
-#                 *) xdotool key --window $i n
-#                    ;;
-#             esac
-#         done
-#     done
-# fi
