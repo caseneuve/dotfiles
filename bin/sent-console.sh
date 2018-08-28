594e722107226e236964ca39dac5bcfe30f3e03d
caseneuve <piotr.kaznowski@gmail.com>
2018-08-29 00:00:11 +0200

    console for SENT added

---
+++ bin/sent-console.sh
@@ -0,0 +1,38 @@
+#!/bin/bash
+
+# Path:        ~/.dotfiles/bin/sent-console.sh
+# Created:     28.08.18, 19:37    @x200
+# Last update: 28.08.18, 23:59:54 @x200
+
+# >> DOC: 
+# simple console to navigate through SENT slides
+
+# >> TODOS: 
+# todo: add counters, eg : 5n, 5p etc.
+# todo: read file, and echo which slide we are currently on 
+
+# >> RUN:
+clear
+echo "Sent console v0.1 alpha"
+
+while true; do
+    SENT_ID=$(xdotool search --class sent)
+    read -p "> " command
+    for i in $SENT_ID; do
+        case $command in
+            a|g|n|p|r) xdotool key --window $i $command
+                       ;;
+            'exit'|'quit') exit
+                       ;;
+            clr|clear|klr) clear; echo "Sent console v0.1 alpha"
+                           ;;
+            *) echo "commands are [n]ext, [p]revious, [r]eload, [a]beg [g]end"
+               break
+               ;;
+        esac
+    done
+done
+
+    
+
+        
