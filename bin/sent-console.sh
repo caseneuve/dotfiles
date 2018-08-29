4affaf541c10de6a9e72363a76ee7e94810b1d02
caseneuve <piotr.kaznowski@gmail.com>
2018-08-29 23:20:48 +0200

    notes added

--- bin/sent-console.sh
+++ bin/sent-console.sh
@@ -2,7 +2,7 @@
 
 # Path:        ~/.dotfiles/bin/sent-console.sh
 # Created:     28.08.18, 19:37    @x200
-# Last update: 29.08.18, 01:10:37 @x200
+# Last update: 29.08.18, 20:43:34 @toshiba
 
 # >> DOC: 
 # simple console to navigate through SENT slides
@@ -10,6 +10,12 @@
 # >> TODOS: 
 # todo: add counters, eg : 5n, 5p etc.
 # todo: read file, and echo which slide we are currently on 
+# licz ile jest przerw między akapitami, tj ile slajdów
+# cat -s $file | grep -c "^\s*$"
+
+# >> VARIABLES:
+# spr. czy podany arg jest plikiem 
+# [[ -f $1 ]] && 
 
 # >> RUN:
 clear
