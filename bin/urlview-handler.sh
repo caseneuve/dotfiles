81ab65905479e2830da3865c0ad19200de297965
caseneuve <piotr.kaznowski@gmail.com>
2018-08-30 16:31:03 +0200

    wget files → dwl to /tmp and open with wget

--- bin/urlview-handler.sh
+++ bin/urlview-handler.sh
@@ -2,7 +2,7 @@
 
 # Path:        ~/.bin/urlview-handler.sh
 # Created:     04.04.18, 11:48    @x200
-# Last update: 29.08.18, 23:25:17 @toshiba
+# Last update: 30.08.18, 16:28:50 @lenovo
 
 # Doc: via Luke Smith github
 # todo: poprawić wget i pdf (nie działa xdg-open dla pdf @toshi!)
@@ -24,8 +24,8 @@ elif echo $1 | grep "googleusercontent.com" >/dev/null; then
 elif echo $mpvFiles | grep -w $ext > /dev/null; then
     [[ -f $mpvscript ]] && nohup $mpvscript "$1" >/dev/null || nohup mpv --loop --quiet "$1" > /dev/null &
 elif echo $wgetFiles | grep -w $ext > /dev/null; then
-    nohup wget "$1" >/dev/null
-    # nohup wget "$1" -o /tmp/file.$ext >/dev/null
+    #nohup wget "$1" >/dev/null
+    nohup wget -O /tmp/file.$ext "$1" >/dev/null && xdg-open /tmp/file.$ext &
     # [[ $ext == "pdf" ]] && mupdf /tmp/file.$ext || xdg-open /tmp/file.$ext &
 elif echo $1 | grep "youtube\.\|youtu\.be" > /dev/null; then
     if [[ -f $mpvscript ]]; then
