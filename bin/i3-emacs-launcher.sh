81cbee3caec93bc5d47947598d008b01c48715ac
caseneuve <piotr.kaznowski@gmail.com>
2018-09-02 00:33:19 +0200

    popr. wersja i3-emacs

--- bin/i3-emacs-launcher.sh
+++ bin/i3-emacs-launcher.sh
@@ -2,44 +2,33 @@
 
 # Path:        ~/.dotfiles/bin/i3-emacs-launcher.sh
 # Created:     27.05.18, 09:16    @x200
-# Last update: 28.08.18, 13:18:33 @x200
+# Last update: 02.09.18, 00:32:16 @lenovo
 
 # >> DOC:
 # "If emacs is running goes to the first workspace with qtb window; if not -- launches qtb at wksp 1"
 # note: requires `wmctrl` package (pacman)
 
-# >> TODOS:
-# done: popr - nie szukaj tytułów dla cli, jeśli nie ma procesu emacsclient
-# fixme: gdy 2 instancje E. na 1 ws, nie togluje (spr mutt)
-# todo: dodać opcje dla większej liczby otwartych okien (niech cyklicznie przechodzi po następnych)
-# todo: przepisać to na py (i3ipc)
-
 # >> VARIABLES:
-APP=emacs
-#WS=$(i3-msg -t get_config | grep 'set \$ws1[^0]' | awk '{print $3 " " $4}')
-WS=$(i3-msg -t get_config | grep 'set \$ws1[^0]' | awk '{print $4}')
-EMACS_GUI=$(wmctrl -lx | grep "emacs.Emacs" | grep -v grep)
-#EMACS_GUI_ID=$(wmctrl -lx | grep "emacs.Emacs" | grep -v grep | awk '{print $1}')
-EMACS_GUI_ID=$(wmctrl -lx | awk '/emacs.Emacs/ && !/st-256color/ {print $1}')
+GUI_ID=$(wmctrl -lx | awk '/emacs.Emacs/ && !/st-256color/ {print $1}')
+CLI_ID=$(wmctrl -lx | grep "emacsclient\| e " | grep -v grep | awk '{print $1}')
+IDS=($GUI_ID $CLI_ID)
+len=${#IDS[@]}
+FOC_NAME=$(~/bin/i3get -n)
+FOC_ID=$(wmctrl -l | grep "$FOC_NAME" | awk  '{print $1}')
 
-[[ $EMACS_GUI ]] && EMACS_GUI_WS=$(echo $EMACS_GUI | awk '{print $2}')
-FOCUSED_WS=$(wmctrl -d | awk '/*/ {print $1}')
-[[ $(ps aux | awk '/emacsclient/ {print $11}') == *'emacsclient'* ]] &&\
-    EMACS_CLI=$(wmctrl -lx | grep "emacsclient\| e " | grep -v grep | awk '{print $1}') ||\
-        EMACS_CLI=
-
-# >> RETURN:
-if [[ $EMACS_GUI ]]; then
-    if [[ $FOCUSED_WS == $EMACS_GUI_WS ]]; then
-        [[ $EMACS_CLI ]] && wmctrl -ia $EMACS_CLI || notify-send -u low "i3" "<i>No other instances of Emacs visible</i>"
+# >> RUN
+if [[ -n $IDS ]]; then
+    if [[ $(echo ${IDS[@]} | grep $FOC_ID) ]]; then
+        for ((i=0; i<len; i++)); do
+            if [[ $FOC_ID == ${IDS[$i]} ]]; then
+                (( len == 1 )) && notify-send "No more Emacs instances active"; break 
+                (( i + 1 < len )) && a=$((i+1)) || a=0
+                wmctrl -ia ${IDS[$a]} && break
+            fi
+        done
     else
-        wmctrl -ia $EMACS_GUI_ID
+        wmctrl -ai ${IDS[0]}
     fi
 else
-    i3-msg -q "workspace $WS; exec --no-startup-id $APP"
+    emacs &
 fi
-
-# >> SPADY:
-# ws=$(i3-msg -t get_tree | grep -oE "num\":[0-9]*||emacs" | tr "\n" " " | grep -o "num\":[0-9]* emacs" | grep -o [0-9]* | head -n 1)
-# [[ "$ws" ]] && i3-msg "workspace $ws" || i3-msg "workspace 1; exec --no-startup-id emacs"
-
