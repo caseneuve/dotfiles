842ecdfeed4e89e124d84b122546fe7afa28de5c
caseneuve <piotr.kaznowski@gmail.com>
2018-09-02 13:20:24 +0200

    outline

--- bin/i3-emacs-launcher.sh
+++ bin/i3-emacs-launcher.sh
@@ -2,7 +2,7 @@
 
 # Path:        ~/.dotfiles/bin/i3-emacs-launcher.sh
 # Created:     27.05.18, 09:16    @x200
-# Last update: 02.09.18, 13:18:49 @lenovo
+# Last update: 02.09.18, 13:20:06 @lenovo
 
 # >> DOC:
 # "If emacs is running goes to the first workspace with qtb window; if not -- launches qtb at wksp 1"
@@ -25,25 +25,25 @@ WSS=(${GUI_WS[@]} ${CLI_WS[@]})
 len=${#IDS[@]}
 
 # >> RUN:
-# 1. if emacs is running running
+# >>  1. if emacs is running running
 if [[ -n $IDS ]]; then
-    # a. if focused window is emacs
+    # >>   a. if focused window is emacs
     if [[ $(echo ${IDS[@]} | grep $FOC_ID) ]]; then
         for ((i=0; i<len; i++)); do
             if [[ $FOC_ID == ${IDS[$i]} ]]; then
-                # a1. if there is only one emacs window on the list
+                # >>    a1. if there is only one emacs window on the list
                 (( len == 1 )) &&\
                     notify-send -u low \
                                 "i3" \
                                 "<i>No more Emacs instances active</i>"
-                # a2. if focus is on the last emacs win on the list
+                # >>    a2. if focus is on the last emacs win on the list
                 # - go to the beginning
                 (( i + 1 < len )) && a=$((i+1)) || a=0
                 wmctrl -ia ${IDS[$a]} && break
             fi
         done
     else
-        # b. if focused window is not emacs:
+        # >>   b. if focused window is not emacs:
         #  - check if there is an emacs on the same wksp
         # if not, go to the first instance (probably GUI)
         for i in ${!WSS[*]}; do
@@ -54,6 +54,6 @@ if [[ -n $IDS ]]; then
         wmctrl -ai ${IDS[0]}
     fi
 else
-    # 2. or run emacs
+    # >>  2. or run emacs
     emacs &
 fi
