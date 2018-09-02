a10dcb8e384c15bb1e3629ec0163f7d4c28c070a
caseneuve <piotr.kaznowski@gmail.com>
2018-09-02 20:18:04 +0200

    term class added

--- rofi/scripts/st-change-color-4all.sh
+++ rofi/scripts/st-change-color-4all.sh
@@ -2,13 +2,14 @@
 
 # Path:        ~/bin/st-change-color-4all.sh
 # Created:     13.07.18, 15:38    @lenovo
-# Last update: 21.08.18, 14:07:51 @x200
+# Last update: 02.09.18, 20:17:51 @lenovo
 
 # Doc: change theme in all st instances simultaneously
 
 ST_WIND=$(xdotool search --class st)
 SCRATCH_WIND=$(xdotool search --class scratch)
-WINDOWS="$ST_WIND $SCRATCH_WIND"
+TERM_WIND=$(xdotool search --class term)
+WINDOWS="$ST_WIND $SCRATCH_WIND $TERM_WIND"
 
 for i in $WINDOWS; do
     xdotool key --window $i ctrl+F6
