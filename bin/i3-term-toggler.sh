440309cdc0ac9b8e4905022f894a4e687fe50851
caseneuve <piotr.kaznowski@gmail.com>
2018-09-02 20:48:21 +0200

    i3 term toggler done

--- bin/i3-term-toggler.sh
+++ bin/i3-term-toggler.sh
@@ -2,52 +2,51 @@
 
 # Path:        ~/.dotfiles/bin/i3-term-toggler.sh
 # Created:     02.09.18, 15:28    @lenovo
-# Last update: 02.09.18, 16:01:40 @lenovo
+# Last update: 02.09.18, 20:47:19 @lenovo
 
-# >> DOC: cycles throug visible terminals (classed "term")
+# >> DOC: cycles through visible terminals (classed "term")
 # requires: xdotool
+# requires: wmctrl 
 
 # >> TODOS: 
+# todo: przepisać wszystko na wmctrl?
 
 # >> VARIABLES: 
-IDS+=$(xdotool search --class term)
+IDS=($(xdotool search --class term))
 FOC_ID=$(xdotool getactivewindow)
-FOC_WS=$(xdotool get_desktop_for_window $FOC_ID)
-declare -a WSS+=$(wmctrl -lx | awk '/st-256color.term/ {print $1}')
-echo $IDS
-echo ${#WSS[@]}
-echo ${WSS[0]}
-#xdotool windowraise $id
+FOC_WS=$(wmctrl -d | awk '/*/ {print $1}')
+WSS=($(wmctrl -lx | awk '/st-256color.term/ {print $2}'))
+len=${#IDS[@]}
 
 # >> RUN: 
 if [[ -n $IDS ]]; then
-    # >>   a. if focused window is emacs
+    # >>   a. if focused window is a term
     if [[ $(echo ${IDS[@]} | grep "$FOC_ID") ]]; then
         for ((i=0; i<len; i++)); do
             if [[ $FOC_ID == ${IDS[$i]} ]]; then
-                # >>    a1. if there is only one emacs window on the list
-                (( len == 1 )) &&\
+                # >>    a1. if there is only one term window on the list
+                (( len == 1 )) && \
                     notify-send -u low \
                                 "i3" \
                                 "<i>No other terminals opened</i>"
-                # >>    a2. if focus is on the last emacs win on the list
+                # >>    a2. if focus is on the last term win on the list
                 # - go to the beginning
                 (( i + 1 < len )) && a=$((i+1)) || a=0
-                xdotool windowraise ${IDS[$a]} && break
+                wmctrl -ia ${IDS[$a]} && break
             fi
         done
     else
-        # >>   b. if focused window is not emacs:
+        # >>   b. if focused window is not term:
         #  - check if there is an emacs on the same wksp
-        # if not, go to the first instance (probably GUI)
+        # if not, go to the first instance
         for i in ${!WSS[*]}; do
             if [[ ${WSS[$i]} = $FOC_WS ]]; then
-                xdotool windowraise ${IDS[$i]} && exit
+                wmctrl -ia ${IDS[$i]} && exit
             fi
         done
-        xdotool windowraise ${IDS[0]}
+        wmctrl -ia ${IDS[0]}
     fi
 else
-    # >>  2. or run emacs
+    # >>  2. or run term
     st -c term &
 fi
