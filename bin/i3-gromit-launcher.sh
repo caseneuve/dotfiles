ad28d93afa75f939aa6655017eae4e2b34a7018a
caseneuve <piotr.kaznowski@gmail.com>
2018-09-03 23:37:49 +0200

    kosmetyka

--- bin/i3-gromit-launcher.sh
+++ bin/i3-gromit-launcher.sh
@@ -2,7 +2,7 @@
 
 # Path:        ~/scr/gromit-launcher.sh
 # Created:     03.09.18, 22:57    @x200
-# Last update: 03.09.18, 23:35:08 @x200
+# Last update: 03.09.18, 23:37:40 @x200
 
 # >> DOC: 
 
@@ -20,17 +20,3 @@ else
     pkill -RTMIN+14 i3blocks
     notify-send -u low "Gromit:" "is now enabled!\nF9     - toggle draw\nShift  - blue\nCtrl   - yellow\nAlt    - erase\nF10    - undo\nwin+F8 - disable"
 fi
-
-# [[ $(ps aux | grep "gromit-mpx" | grep -v grep) ]] && 
-
-# >> RUN:
-# while getopts 'sk' flag; do
-#     case "${flag}" in
-#         s) gromit-mpx &
-#            [[ $(ps aux | grep "gromit-mpx" | grep -v grep) ]] && pkill -RTMIN+14 i3blocks
-#            ;;
-#         k) kill -9 $G_PID
-#            pkill -RTMIN+14 i3blocks
-#            ;;
-#     esac
-# done
