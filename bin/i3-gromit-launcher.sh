955cd5511f97ea9bad5700d6ee4bb288794b74ec
caseneuve <piotr.kaznowski@gmail.com>
2018-09-03 23:42:07 +0200

    activate gromit on start

--- bin/i3-gromit-launcher.sh
+++ bin/i3-gromit-launcher.sh
@@ -2,7 +2,7 @@
 
 # Path:        ~/scr/gromit-launcher.sh
 # Created:     03.09.18, 22:57    @x200
-# Last update: 03.09.18, 23:37:40 @x200
+# Last update: 03.09.18, 23:41:20 @x200
 
 # >> DOC: 
 
@@ -16,7 +16,7 @@ if [[ $G_PID ]]; then
     pkill -RTMIN+14 i3blocks
     notify-send -u critical "Gromit:" "has been disabled"
 else
-    gromit-mpx &
+    gromit-mpx -a &
     pkill -RTMIN+14 i3blocks
     notify-send -u low "Gromit:" "is now enabled!\nF9     - toggle draw\nShift  - blue\nCtrl   - yellow\nAlt    - erase\nF10    - undo\nwin+F8 - disable"
 fi
