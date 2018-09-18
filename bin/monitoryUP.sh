48a643ad4207a74c56ff5f2111e90b79b1511553
caseneuve <piotr.kaznowski@gmail.com>
2018-09-18 18:36:43 +0200

    cd.

--- bin/monitoryUP.sh
+++ bin/monitoryUP.sh
@@ -2,16 +2,21 @@
 
 # Path:        ~/.dotfiles/bin/monitory-UP.sh
 # Created:     06.07.18, 09:47    @toshiba
-# Last update: 18.09.18, 10:44:20 @x200
+# Last update: 18.09.18, 14:23:54 @x200
 
 # Doc: default setting for 2 monitors (primary down, secondary up)
 #xrandr --current | grep -A 1 primary | awk -F"x" 'NR==2 {gsub(/^ */,""); print $1}'
+
 intern=$(xrandr --listmonitors | awk 'NR==2 {print $(NF)}')
 #intern=$(xrandr --current | awk 'NR==2 {print $1}')
+
 inres=$(xrandr --current | grep "+$\|+ " | awk ' NR==1 {print $1}')
+
+[[ $(xrandr --listmonitors | awk 'NR==1 {print $2}') > 1 ]] &&\
+    extern=$(xrandr --listmonitors | awk 'NR==3 {print $(NF)}') ||\
+        extern=$(xrandr --current | grep -v $intern | grep -A 1 " connected" | awk 'NR==1 {print $1}')
 #extern=$(xrandr | grep " connected" | awk 'NR==2 {print $1}')
-#extern=$(xrandr --current | grep -v $intern | grep -A 1 " connected" | awk '{print $1}')
-extern=$(xrandr --listmonitors | awk 'NR==3 {print $(NF)}')
+
 exres=$(xrandr --current | grep "+$\|+ " | awk ' NR==2 {print $1}')
 posy=$(echo $exres | sed 's/[0-9]*x//')
 
@@ -21,5 +26,7 @@ else
     xrandr --output "$intern" --primary --mode "$inres" --pos 0x$posy --rotate normal --output "$extern" --mode "$exres" --pos 0x0 --rotate normal
 fi
 
-~/.fehbg
-pkill dunst && dunst &
+if [ -n $extern ] && [ -n $intern ]; then
+    ~/.fehbg;
+    pkill dunst && dunst &
+fi
