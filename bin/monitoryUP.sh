5e18d6db6f58df7fac990492868171bb9282cc96
caseneuve <piotr.kaznowski@gmail.com>
2018-09-18 09:49:32 +0200

    monitory up popr. bug dla extern

--- bin/monitoryUP.sh
+++ bin/monitoryUP.sh
@@ -2,15 +2,16 @@
 
 # Path:        ~/.dotfiles/bin/monitory-UP.sh
 # Created:     06.07.18, 09:47    @toshiba
-# Last update: 16.07.18, 19:05:02 @x200
+# Last update: 18.09.18, 08:06:15 @x200
 
 # Doc: default setting for 2 monitors (primary down, secondary up)
 #xrandr --current | grep -A 1 primary | awk -F"x" 'NR==2 {gsub(/^ */,""); print $1}'
 intern=$(xrandr --listmonitors | awk 'NR==2 {print $(NF)}')
 #intern=$(xrandr --current | awk 'NR==2 {print $1}')
 inres=$(xrandr --current | grep "+$\|+ " | awk ' NR==1 {print $1}')
-extern=$(xrandr | grep " connected" | awk 'NR==2 {print $1}')
+#extern=$(xrandr | grep " connected" | awk 'NR==2 {print $1}')
 #extern=$(xrandr --current | grep -v $intern | grep -A 1 " connected" | awk '{print $1}')
+extern=$(xrandr --listmonitors | awk 'NR==3 {print $(NF)}')
 exres=$(xrandr --current | grep "+$\|+ " | awk ' NR==2 {print $1}')
 posy=$(echo $exres | sed 's/[0-9]*x//')
 
