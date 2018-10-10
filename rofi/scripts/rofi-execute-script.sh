d70a50b7fce75d30a9b7aec7ee76c126c463b05b
caseneuve <piotr.kaznowski@gmail.com>
2018-10-10 15:43:36 +0200

    support for terminal scripts added

--- rofi/scripts/rofi-execute-script.sh
+++ rofi/scripts/rofi-execute-script.sh
@@ -2,11 +2,15 @@
 
 # Path:        ~/.dotfiles/rofi/scripts/rofi-execute-script.sh
 # Created:     13.07.18, 21:47    @x200
-# Last update: 19.07.18, 01:17:59 @x200
+# Last update: 10.10.18, 15:42:36 @lenovo
 
 # Doc:
+st_list="mupdf-console" # script to open in terminal
+script_dir=~/.config/rofi/scripts/
+selected=$(ls ~/.config/rofi/scripts/ | grep -v execute | rofi -theme mytheme -dmenu -p 'choose a script to execute: ')
 
-selected=$(ls ~/.config/rofi/scripts/ | rofi -theme mytheme -dmenu -p 'choose a script to execute: ')
-
-~/.config/rofi/scripts/$selected
-
+if echo $st_list | grep -w $selected > /dev/null; then
+    st -e $script_dir$selected
+else
+    $script_dir$selected
+fi
