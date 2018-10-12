227137c2a263969aebf49b53c00065a3b7b5e760
caseneuve <piotr.kaznowski@gmail.com>
2018-10-13 01:42:46 +0200

    skrypty: combo dla scratchpadów

--- bin/i3quick-floating.sh
+++ bin/i3quick-floating.sh
@@ -2,7 +2,7 @@
 
 # Path:        ~/.dotfiles/bin/i3quick-floating.sh
 # Created:     25.08.18, 16:57    @toshiba
-# Last update: 25.08.18, 16:57:52 @toshiba
+# Last update: 13.10.18, 01:42:07 @lenovo
 
 # >> DOC: 
 
@@ -10,7 +10,11 @@
 
 # >> VARIABLES: 
 
-# >> RUN: 
-i3-msg -q '[instance="myquickterm"] scratchpad show'
-~/bin/i3move -s -m 1
+# >> RUN:
 
+if [[ $(ps aux | grep myquickterm | grep -v grep) ]]; then
+    i3-msg -q '[instance="myquickterm"] scratchpad show' && ~/bin/i3move -s -m 1
+else
+    i3-msg -q "exec --no-startup-id st -c 'scratch' -n myquickterm -f 'Inconsolata:size=13'" && sleep 0.05
+    i3-msg -q '[instance="myquickterm"] scratchpad show' && ~/bin/i3move -s -m 1
+fi
