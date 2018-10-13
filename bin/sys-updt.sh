915b63c263b1becf6c53e2cebb74189cdd3d103b
caseneuve <piotr.kaznowski@gmail.com>
2018-10-13 21:15:29 +0200

    sys-updt with support of i3move -p

--- bin/sys-updt.sh
+++ bin/sys-updt.sh
@@ -2,14 +2,12 @@
 
 # Path:        ~/.dotfiles/bin/sys-updt.sh
 # Created:     30.05.18, 16:02    @x200
-# Last update: 25.08.18, 21:21:54 @toshiba
+# Last update: 13.10.18, 21:15:14 @lenovo
 
 # >> DOC: Primitive pamac-tray replacement
 
 # >> TODOs
 # note: uses pacman-contrib package! (`checkupdates` command)
-# check: spr. czy $DISTRO działa na wszystkich maszynach 
-# note: zmieniam DISTRO na antergos (skoro wszędzie to samo)
 # done: ?? po kliknięciu (lewy lub prawy) i3blocks się zwiesza
 # done: dodać liczenie ile updatów do ikonki
 
@@ -20,7 +18,6 @@ checkupdates > $FILE
 DIR=~/.dotfiles/i3/bin
 CHECK=$(cat $FILE)
 NUM=$(cat $FILE | wc -l)
-DISTRO=antergos
 GLYPH=''
 COLOR='#C0392B'
 
@@ -34,37 +31,15 @@ fi
 case $BLOCK_BUTTON in
     # left click 
     1) notify-send -u critical "System update:
---------------" "$CHECK" -i $DIR/$DISTRO-logo.png
+--------------" "$CHECK" 
        pkill -RTMIN+12 i3blocks && exit 0
        ;;
     # right click
-    3) st -t sysupdt -e sudo pacman --noconfirm -Syyu
+    3) rm $FILE
+       st -t sysupdt -e sudo pacman --noconfirm -Syyu
+       i3move -p 35 -g 10 -m ne
        pkill -RTMIN+12 i3blocks
        exit 0
        ;;
 esac
-
-# >> SPADY
-#redbg=`tput setab 1`
-#nor=`tput sgr0`
-# DISTRO=`cat /etc/*-release | grep ^ID= | sed 's/^ID=\"\|\"\|ID=//g'`
-# DISTRO=`cat /etc/issue | awk '{print tolower($1)}' | head -n 1`
-
-# while getopts 'nry' flag; do
-#     case "${flag}" in
-#         n) [[ -z "$CHECK" ]] && notify-send -u low "System update:
-# --------------" "System is up to date :-)" -i $DIR/$DISTRO-logo.png || notify-send -u critical "System update:
-# --------------" "$CHECK" -i $DIR/$DISTRO-logo.png && exit 0;;
-#         r) i3-msg -q "exec --no-startup-id st -t sysupdt -e sudo pacman --noconfirm -Syyu"
-#            pkill -RTMIN+12 i3blocks && exit 0;;
-#         y) i3-msg -q "exec --no-startup-id st -t sysupdt -e yaourt --noconfirm -Syu --aur" && exit 0;;
-#         # h) print_help;;
-#         *) main;;
-#     esac
-# done
-           
-# print_help(){
-# printf "sys-updt = system update script
-#  -n              outpusend notification"
-# }
 
