692da09452f86713860699756195902bde089a9c
caseneuve <piotr.kaznowski@gmail.com>
2018-10-13 21:16:07 +0200

    path popr.

--- bin/sys-updt.sh
+++ bin/sys-updt.sh
@@ -2,7 +2,7 @@
 
 # Path:        ~/.dotfiles/bin/sys-updt.sh
 # Created:     30.05.18, 16:02    @x200
-# Last update: 13.10.18, 21:15:14 @lenovo
+# Last update: 13.10.18, 21:16:03 @lenovo
 
 # >> DOC: Primitive pamac-tray replacement
 
@@ -37,7 +37,7 @@ case $BLOCK_BUTTON in
     # right click
     3) rm $FILE
        st -t sysupdt -e sudo pacman --noconfirm -Syyu
-       i3move -p 35 -g 10 -m ne
+       ~/bin/i3move -p 35 -g 10 -m ne
        pkill -RTMIN+12 i3blocks
        exit 0
        ;;
