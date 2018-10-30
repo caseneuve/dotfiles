73665cc2fd7037b3a81df05dbc085a6afc4f14a9
caseneuve <piotr.kaznowski@gmail.com>
2018-10-30 10:39:06 +0100

    poprawiony match dla class

--- bin/i3startterminemacsdir.sh
+++ bin/i3startterminemacsdir.sh
@@ -2,7 +2,8 @@
 
 # Path:        ~/biu/bash/cdemacsdir.sh
 # Created:     26.08.18, 15:00    @x200
-# Last update: 29.10.18, 20:07:56 @lenovo
+# Last update: 30.10.18, 10:38:51 @x200
+
 
 # >> DOC:
 # Requires:
@@ -13,6 +14,7 @@
 # open the terminal in the working directory; 
 # else, open in $HOME.
 
+
 # >> VARIABLES:
 FULL=$(wmctrl -lx | awk '/emacs.Emacs/ {print $5}')
 FILE=$(basename "$FULL")
@@ -20,9 +22,8 @@ DIR=${FULL%$FILE}
 FOC=$(~/git/hub/i3/i3get.sh -c)
 
 # >> RUN:
-if [[ $FOC == 'Emacs' && -d $DIR ]]; then
+if [[ $FOC =~ "Emacs" && -d $DIR ]]; then
     cd $DIR && st -c term -t "st @$(date +%H:%M:%S)" &
 else
     st -c term -t "st @$(date +%H:%M:%S)" &
 fi
-
