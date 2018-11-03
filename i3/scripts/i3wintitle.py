28f13e6010d468297d66b5c40a4721d52156f8bc
caseneuve <piotr.kaznowski@gmail.com>
2018-11-03 10:24:49 +0100

    unicodedata imported: kill control chars inside titles

--- i3/scripts/i3wintitle.py
+++ i3/scripts/i3wintitle.py
@@ -11,17 +11,7 @@
 # - [ ] mpv
 # >> TODO: czy da się dostać do tmuxa?; note: tak, ale nie wiem jak updatować skrypt, gdy już się jest w tmuksie
 # >> TODO: glyphs
-# lista:
-# obrazy: gpick, gimp, mypaint
-# wideo: mpv, vlc
-# www: chrome, chromium, firefox
-# dirs: thunar, ranger
-# mail: neomutt, mutt
-# social: slack, hangups?
-# calendar: calcurse
-# calc: R
-# wifi: (netmanager?)
-# py? (ipython)
+
 
 # DONE: gdy zmienia się okno emacsa nie emituje się zmiana sygnału, więc pozostaje pierwszy tytuł (trzeba było dodać `i3.on("window::title", on_window_pkill)` do i3listen.py), note:05/08/2018
 # DONE: make a dict {('st': icon), ('emacs': icon)}, NOTE: 05/08/2018, trzeba tylko ściągnąć odpowiednie glify
@@ -29,11 +19,14 @@
 # DONE: make a script that listens to i3 and responds when window changes focus
 # DONE: shorten name, note: 05/08/2018
 
+
 # >> IMPORTS
 #from subprocess import check_output as out
 import re
+import unicodedata
 from subprocess import check_output as out
 import i3ipc
+
 
 # >> VARIABLES
 i3 = i3ipc.Connection()
@@ -42,32 +35,38 @@ focused = tree.find_focused()
 i3class = focused.window_class
 i3name = focused.name
 
+
 # >> IF FOCUS IS ON EMPTY WORKSPACE, RESET THE TITLE
 if i3class == None:
     i3name = ''
     i3class = ''
+
 
 # >> MUPDF: GET PAGE NUMBER FIRST
 mupdfRe = re.compile(r'([a-z0-9 ._-]*)(.pdf - )([0-9]+/[0-9]+)', re.I)
 if i3class == 'MuPDF':
     i3name = f'[{mupdfRe.search(i3name).group(3)}] {mupdfRe.search(i3name).group(1)}'
 
+
 # >> Zathura: GET GET PAGE NUMBER FIRST
 zathuraRe = re.compile(r'([a-z0-9 ._-]*)(.pdf ).([0-9]+/[0-9]+).', re.I)
 if i3class == 'Zathura':
     i3name = f'[{zathuraRe.search(i3name).group(3)}] {zathuraRe.search(i3name).group(1)}'
-    
+
+
 # >> SENT: find file name
 if 'sent' in i3name and 'presenter' in i3class:
     sent_pid = out(['pidof', 'sent']).decode('utf-8').strip()
     i3name = out(['ps', sent_pid]).decode('utf-8').strip().split()[10]
+
 
 # >> EMACS: EXTRACT ONLY BASENAME
 if i3class == 'Emacs':
     if '/' in i3name:
         i3name = out(['basename', i3name]).decode('utf-8').strip()
 
-        # >> CLASS ICONS DICT
+
+# >> CLASS ICONS DICT
 class_dict = {'Emacs': '',
               'MuPDF': '',
               'Zathura': '*',
@@ -96,6 +95,7 @@ class_dict = {'Emacs': '',
               'cl0ck': '',
               }
 
+
 # >> CHANGE ICON DICT
 if i3class in class_dict:
     i3class = class_dict[i3class]
@@ -110,6 +110,7 @@ name_dict = {'neomutt': '',
 if i3name in name_dict:
     i3class = name_dict[i3name]
 
+
 # >> CHANGE NAME DICT
 special_dict = {'dropdown': 'tmux',
                 'm4ath': 'R calc',
@@ -119,23 +120,20 @@ special_dict = {'dropdown': 'tmux',
 if i3name in special_dict:
     i3name = special_dict[i3name]
 
+
 # >> REMOVE SOME UNNECESARY INFO
 removeRe = re.compile(
     r' - qutebrowser|sxiv -|feh|^Firefox |^Home \| |\| GitLab|Slack - |- Mozilla Firefox')
 if removeRe.search(i3name):
     i3name = re.sub(removeRe, '', i3name)
 
+
 # >> SHORTEN WINDOW NAME
 if len(i3name) > 50:
     i3name = i3name[0:50] + "..."
 
-# >> RETURN string for i3blocks
-# skoryguj niedozwolone znaki
-# for i in ['\n', '&']:
-#     if i in i3name:
-#         i3name = 'shell command'
 
-# >> tests for pango:
+# >> REPLACE invalid pango characters
 pango_escape = {
     "&": "&amp;",
     "<": "&lt;",
@@ -145,14 +143,11 @@ for i in pango_escape:
     if i in i3name:
         i3name = i3name.replace(i, pango_escape[i])
 
-print(f"<span weight='normal'>{i3class}  {i3name}</span>")
-#print('{:300}'.format(i3class+"  "+i3name))
-#print(f"<span weight='bold'>{i3name}</span>")
 
-# >> SPADY:
-# >> TMUX
-# if 'dropdown' in i3name:
-#     tmux_windows = out(['tmux', 'list-windows']).decode('utf-8').split('\n')
-#     for name in tmux_windows:
-#         if "active" in name:
-#             i3name = 'tmux: ' + name.split(' ')[1][:-1]
+# >> KILL control characters (invalid in JSON string)
+i3name = "".join(ch for ch in i3name if unicodedata.category(ch)[0]!="C")
+
+
+# >> PRINT result
+print(f"<span weight='normal'>{i3class}  {i3name}</span>")
+
