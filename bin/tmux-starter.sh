cf5fb331965da797fd38183eeb90339862b2709c
caseneuve <piotr.kaznowski@gmail.com>
2018-11-03 07:29:40 +0100

    condition added (dont load new session when one is already active)

--- bin/tmux-starter.sh
+++ bin/tmux-starter.sh
@@ -2,22 +2,26 @@
 
 # Path:        ~/.tmux/tmux-hg.sh
 # Created:     13.03.18, 10:30    @x200
-# Last update: 16.08.18, 21:01:55 @x200
+# Last update: 02.11.18, 22:11:15 @x200
 
 # Doc: starts tmux with 3 windows, the second one named "hg" with HANGUPS and the third "cal" with CALCURSE running
 # https://stackoverflow.com/questions/5609192/how-to-set-up-tmux-so-that-it-starts-up-with-specified-windows-opened
 
-tmux new-session -s 'tmux' -d 
-tmux new-window -n 'hg' 'hangups'
-tmux new-window -n 'rng' 'ranger'
-tmux new-window -n 'cal' 'calcurse'
-tmux new-window -n 'mocp' 'mocp'
-tmux new-window -n 'htop' 'htop'
-tmux rename-window -t%0 'term'
-tmux select-window -t 'tmux:term'
-tmux splitw -h -p 20
-tmux clock-mode
-tmux select-pane -L
-#tmux select-window -t 'tmux:cal'
-tmux -2 attach -d
+if [[ ! $(tmux ls | grep tmux ) ]]; then
+    tmux new-session -s 'tmux' -d 
+    tmux new-window -n 'hg' 'hangups'
+    tmux new-window -n 'rng' 'ranger'
+    tmux new-window -n 'cal' 'calcurse'
+    tmux new-window -n 'mocp' 'mocp'
+    tmux new-window -n 'htop' 'htop'
+    tmux rename-window -t%0 'term'
+    tmux select-window -t 'tmux:term'
+    tmux splitw -h -p 20
+    tmux clock-mode
+    tmux select-pane -L
+    #tmux select-window -t 'tmux:cal'
+    tmux -2 attach -d
+else
+    tmux -2 attach -d
+fi
 
