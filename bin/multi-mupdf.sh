08ecbae25487b362d45ae53f330af20d70223e87
caseneuve <piotr.kaznowski@gmail.com>
2018-11-05 23:26:33 +0100

    path - corr. + added opt for files with spaces in the name

--- bin/multi-mupdf.sh
+++ bin/multi-mupdf.sh
@@ -2,17 +2,31 @@
 
 # Path:        ~/scr/multi-mupdf.sh
 # Created:     05.11.18, 22:44    @lenovo
-# Last update: 05.11.18, 22:46:44 @lenovo
+# Last update: 05.11.18, 23:26:23 @lenovo
 
 # >> DOC: 
+# Open multiple marked pdf files.
 
 # >> TODOS: 
 
 # >> VARIABLES: 
+mu_files="pdf jpg png epub"
+combo=
 
 # >> RUN:
 if [[ -n $@ ]]; then
     for file in $@; do
-        mupdf $file &
+        ext="${file##*.}"
+        if echo $mu_files | grep -w $ext  > /dev/null ; then
+            if [[ -f "$combo$file" ]]; then
+                mupdf "$combo$file" &
+                combo=
+            fi
+        else
+            if [[ ! -f "$file" ]]; then
+                combo="$combo$file "
+            fi
+        fi
     done
 fi
+
