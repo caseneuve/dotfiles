4633c6b3eeebb7c02125f3a094344d863f4f5027
caseneuve <piotr.kaznowski@gmail.com>
2018-11-24 11:48:53 +0100

    test: check num of monitors and use dedicated scripts

--- i3/i3lock/i3betterlock.sh
+++ i3/i3lock/i3betterlock.sh
@@ -2,15 +2,23 @@
 
 # Path:        ~/.dotfiles/i3/i3lock/i3betterlock.sh
 # Created:     25.07.18, 21:04    @x200
-# Last update: 25.07.18, 21:42:31 @x200
+# Last update: 24.11.18, 11:00:32 @x200
 
 # Doc: lock screen (yaourt betterlockscreen)
 # https://github.com/pavanjadhaw/betterlockscreen
+# https://github.com/guimeira/i3lock-fancy-multimonitor
 
+log_file=/tmp/monit_num
+. $log_file
 
 [[ $(mocp -Q %state) = "PLAY" ]] && mocp -P
 SOC=/tmp/mpv
 [[ $(~/bin/mpv-socket -s) = "PLAY" ]] && echo "{ \"command\": [ \"cycle\", \"pause\" ] }" | socat - $SOC 2>/dev/null
 
-betterlockscreen -l dim
+if [[ $num > 1 ]];
+then
+    i3lock-fancy -n
+else
+    betterlockscreen -l dim
+fi
 
