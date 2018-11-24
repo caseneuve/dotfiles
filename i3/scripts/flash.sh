c6e8b7beda5d071eae2029ee8206bcbf4cb0a96b
caseneuve <piotr.kaznowski@gmail.com>
2018-11-24 11:48:23 +0100

    kosm

--- i3/scripts/flash.sh
+++ i3/scripts/flash.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
+# Last update: 24.11.18, 08:36:59 @x200
 
+# DOC:
 # zmienia opacity dla bieżącego okna, skrypt za Luke Smith
 # https://www.youtube.com/watch?v=Ja-DgvGPDu8
 # zmieniłem ostani wiersz, żeby wracał do ustawionej w comptonie opacity
@@ -11,7 +13,4 @@ sleep .1
 transset-df -a -m 0
 sleep .1
 transset-df -a -m 0.85
-
-#sleep .1
-#transset-df -a -m 0.8
 
