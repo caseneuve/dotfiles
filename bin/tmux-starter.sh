6d9714b270e27d6f23506920e59b28c607177455
caseneuve <piotr.kaznowski@gmail.com>
2018-11-25 00:03:30 +0100

    source py env before running weechat

--- bin/tmux-starter.sh
+++ bin/tmux-starter.sh
@@ -2,13 +2,14 @@
 
 # Path:        ~/.tmux/tmux-hg.sh
 # Created:     13.03.18, 10:30    @x200
-# Last update: 24.11.18, 20:41:40 @x200
+# Last update: 24.11.18, 22:09:07 @x200
 
 # Doc: starts tmux with 3 windows, the second one named "hg" with HANGUPS and the third "cal" with CALCURSE running
 # https://stackoverflow.com/questions/5609192/how-to-set-up-tmux-so-that-it-starts-up-with-specified-windows-opened
 
 if [[ ! $(tmux ls | grep tmux ) ]]; then
-    tmux new-session -s 'tmux' -d 
+    tmux new-session -s 'tmux' -d
+    source ~/.virtualenvs/weechatpy2/bin/activate
     #    tmux new-window -n 'hg' 'hangups'
     tmux new-window -n 'wee' 'weechat'
     tmux new-window -n 'rng' 'ranger'
