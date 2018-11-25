44a81097bdd9c7eeefa4d665c9e46a320d2d5c26
caseneuve <piotr.kaznowski@gmail.com>
2018-11-25 10:22:30 +0100

    kor.

--- weechat/weechat_setup.sh
+++ weechat/weechat_setup.sh
@@ -2,7 +2,7 @@
 
 # Path:        ~/.dotfiles/weechat/weechat_setup.sh
 # Created:     24.11.18, 23:39    @x200
-# Last update: 25.11.18, 00:57:07 @x200
+# Last update: 25.11.18, 10:04:28 @lenovo
 
 # >> DOC: 
 
@@ -21,65 +21,72 @@ clear
 echo "===================="
 echo "## WEECHAT SETUP! ##"
 echo ""
-read -p "Confirm that you are running this script as sudo [y/n] " confirm
-if [[ $confirm != y ]]; then
-    echo "Run this script as root!"
-    exit 1
-fi
+# read -p "Confirm that you are running this script as sudo [y/n] " confirm
+# if [[ $confirm != y ]]; then
+#     echo "Run this script as root!"
+#     exit 1
+# fi
 
-echo "Checking dependencies..."
-echo "... Installing:"
-for app in python2 python2-virtualenv weechat; do
-    if [[ $(pacman -Q $app 2>/dev/null) ]]; then
-        echo "... $app"
-        pacman --no-confirm -S $app
+echo "# Checking dependencies..."
+echo "# ... Installing:"
+for app in python2 python2-virtualenv weechat
+do
+    if [[ ! $(pacman -Q $app 2>/dev/null) ]]; then
+        echo ""
+        echo "# ... $app"
+        yay -S $app
+    else
+        echo ""
+        echo "# ... skipping $app"
+    fi
 done
 
-echo "... lua-cjson"
+echo "# ... lua-cjson"
 yay lua-cjson
 
-echo "Running weechat just to build directory structure..."
+echo "# Running weechat just to build directory structure..."
 weechat &
 sleep 2
-pkill weechat
+ps aux | grep "weechat" | grep -v grep | grep -v $0 | awk '{print $2}' | xargs kill -9
 
-echo "Making a python virtual environment in $VIRTWEE..."
-[[ -d $VENVS ]] || echo "... mkd $VIRTWEE"; mkdir $VENVS
+echo "# Making a python virtual environment in $VIRTWEE..."
+[[ -d $VENVS ]] || echo -e "\n# ... mkd $VIRTWEE"; mkdir $VENVS
 cd $VENVS
-[[ -d $VIRTWEE ]] || "... running virtualenv2"; virtualenv2 $VIRTWEE
+[[ -d $VIRTWEE ]] || echo -e "\n#... running virtualenv2"; virtualenv2 $VIRTWEE
 cd $VIRTWEE
-source ./bin/activate
-echo "... installing pync and websocket-client"
+source $VIRTWEE/bin/activate
+echo "# ... installing pync and websocket-client"
 pip install pync
 pip install websocket-client
-echo "... done!"
+echo "# ... done!"
 echo ""
-echo "Installing some plugins: 
-- *matrix-protocol*, 
-- *multiline.pl* 
-- *wee_slack.py*"
-echo -e "(more info
-- https://github.com/torhve/weechat-matrix-protocol-script
-- https://github.com/wee-slack/wee-slack 
-- https://weechat.org/scripts/source/multiline.pl.html/ )"
+echo "# Installing some plugins: 
+  - *matrix-protocol*, 
+  - *multiline.pl* 
+  - *wee_slack.py*"
+echo "# (more info
+  - https://github.com/torhve/weechat-matrix-protocol-script
+  - https://github.com/wee-slack/wee-slack 
+  - https://weechat.org/scripts/source/multiline.pl.html/ )"
 
 cd /tmp
-echo "... cloning matrix-protocol"
+echo -e "\n# ... cloning matrix-protocol"
 git clone https://github.com/torhve/weechat-matrix-protocol-script.git
-echo "... downloading multiline"
+echo -e "\n# ... downloading multiline"
 wget https://weechat.org/files/scripts/multiline.pl
-echo "... downloading wee_slack"
+echo -e "\n# ... downloading wee_slack"
 wget https://raw.githubusercontent.com/wee-slack/wee-slack/master/wee_slack.py
-echo "... moving files to [1] $WEELUA , [2] $WEEPERL and [3] $WEEPY respectively"
-cp weechat-matrix-protocol-script/matrix.lua $WEELUA/ && echo "... [1] done"
-cp multiline.pl $WEEPERL/ && echo "... [2] done"
-cp wee_slack.py $WEEPY/ && echo "... [3] done"
+echo -e "\n# ... moving files to [1] $WEELUA , [2] $WEEPERL and [3] $WEEPY respectively"
+cp /tmp/weechat-matrix-protocol-script/matrix.lua $WEELUA/ && echo -e "\n# ... [1] done"
+cp /tmp/multiline.pl $WEEPERL/ && echo -e "\n# ... [2] done"
+cp /tmp/wee_slack.py $WEEPY/ && echo -e "\n# ... [3] done"
 
-echo "... linking files to autoload folders"
+echo -e "\n# ... linking files to autoload folders"
 ln -s $WEELUA/matrix.lua $WEELUA/autoload/
 ln -s $WEEPERL/multiline.pl $WEEPERL/autoload/
 ln -s $WEEPY/wee_slack.py $WEEPY/autoload/
-echo "... done!"
+echo ""
+echo "# ... done!"
 echo ""
 cat << EOF
 ## instructions ##
