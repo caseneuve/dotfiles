2601e52a6a72b30d64b72c73728a3faccb2b255c
caseneuve <piotr.kaznowski@gmail.com>
2018-12-07 14:27:38 +0100

    check if there is an existing git repo first

--- bin/gitlabinit.sh
+++ bin/gitlabinit.sh
@@ -2,16 +2,33 @@
 
 # Path:        ~/.dotfiles/bin/gitlabinit.sh
 # Created:     17.11.18, 19:53    @lenovo
-# Last update: 17.11.18, 20:22:52 @lenovo
+# Last update: 07.12.18, 14:14:08 @lenovo
 
 # >> DOC:
 # init nonexistent repo on gitlab
 
-# >> VARIABLES: 
+# >> VARIABLES:
+usage(){
+    cat << EOF
+ INIT A GITLAB REPOSITORY IN EXISTING DIRECTORY
+ ==============================================
+ usage: ${0##*/} [proj. name] [namespace]
+
+ > project name  defaults to working directory
+ > namespace     defaults to 'caseneuve'
+
+EOF
+    exit 0
+}
+
 if [[ -z $1 ]]; then
     PROJECT_NAME=${PWD##*/}
 else
-    PROJECT_NAME=$1
+    if [[ $1 =~ -h|--help ]]; then
+        usage
+    else
+        PROJECT_NAME=$1
+    fi
 fi
 
 if [[ -z $2 ]]; then
@@ -29,7 +46,7 @@ while true; do
 >
 >  Proceed? [Y/n] " answer
     if [[ $answer =~ y|Y ]] || [[ -z $answer ]]; then
-        git init
+        [[ $(git rev-parse --is-inside-work-tree 2>/dev/null) ]] && git init
         git add .
         git commit -m 'initial commit'
         git push --set-upstream git@gitlab.com:$NAMESPACE/$PROJECT_NAME.git master
