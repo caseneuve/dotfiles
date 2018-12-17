b8a58711e36bf613cc36e0296d74b8193c7a2042
caseneuve <piotr.kaznowski@gmail.com>
2018-12-17 19:37:48 +0100

    messenger-qtb, desktop/ added

---
+++ bin/messenger-qtb.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+# Path:        ~/.dotfiles/bin/messenger-qtb.sh
+# Created:     17.12.18, 19:31    @lenovo
+# Last update: 17.12.18, 19:33:16 @lenovo
+
+# >> DOC:
+# Run messenger in a separate window of a qutebrowser instance
+# May be run with rofi (check messenger.desktop)
+
+# >> RUN:
+qutebrowser :command ":open -w https://messenger.com"
+
