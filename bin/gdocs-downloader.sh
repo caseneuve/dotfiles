10af07476c23b9d08a0f13d2bb5edb3ed29e726c
caseneuve <piotr.kaznowski@gmail.com>
2018-12-28 16:38:07 +0100

    gdocs downloader added

---
+++ bin/gdocs-downloader.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+
+# Path:        ~/scr/gdocs.sh
+# Created:     28.12.18, 15:20    @x200
+# Last update: 28.12.18, 16:35:20 @x200
+
+# >> DOC: 
+# Downloads file from Google Docs in a chosen format (html or txt); when 
+
+# >> VARIABLES: 
+OUTDIR="$HOME/dwl/gdoc"
+ROFI_ARGS="-theme i3orange -monitor -2 -dmenu -p"
+
+# >> RUN:
+answ=$(echo -e "tak\nnie" | rofi $ROFI_ARGS "Zapisać plik na dysku? ")
+if [[ $answ =~ n ]]; then
+    qutebrowser "$1" > /dev/null &
+else
+    form=$(echo -e "html\ntxt" | rofi $ROFI_ARGS "Wybierz format: ")
+    LINK="${1%/*}/export?format=$form"
+    FILE=$(date '+%y%m%d_%H%M%S_GDocs')
+    wget $LINK -O ~/dwl/gdoc/$FILE.$form > /dev/null
+    if [[ $form == html ]]; then
+        pandoc -o "$OUTDIR/$FILE.org" -f html -t org "$OUTDIR/$FILE.html" > /dev/null
+        [[ -f ~/dwl/gdoc/$FILE.org ]] && notify-send "URLVIEW" "File $FILE.$form downloaded and converted to ORG!"
+    else
+        [[ -f ~/dwl/gdoc/$FILE.$form ]] && notify-send "URLVIEW" "File $FILE.$form downloaded!"
+    fi
+fi
+
