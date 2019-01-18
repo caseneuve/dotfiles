36fab1e9649a4dc62012b0ba88ce6b3153615ab3
caseneuve <piotr.kaznowski@gmail.com>
2019-01-18 21:01:19 +0100

    date format change

--- bin/sendmailquick.sh
+++ bin/sendmailquick.sh
@@ -37,7 +37,7 @@ if [ -z "$1" ]; then
     echo "${red}${bold}Nie zaznaczyłeś załączników!${normal}";
 else
     echo -e "Quickmail sent via RANGER, $(date '+%d/%m/%Y, %H:%M:%S')\n-----------------------------------------------\nDo:     $mailddrs\nAtt:    $@\n" > $MAILDIR/ranger_quicksent-$(date +%y%m%dT%H%M)
-    echo -e "Wiadomość wysłana automatycznie $(date '+%d/%m o %H:%M'), zawiera następujące pliki:\n$(echo $@ | tr ' ' '\n')\n\n" | neomutt -s "[rng] $1$ATT" $mailddrs -a "$@" > /dev/null 2>&1 &&\
+    echo -e "Wiadomość wysłana automatycznie $(date '+%d.%m o %H:%M'), zawiera następujące pliki:\n$(echo $@ | tr ' ' '\n')\n\n" | neomutt -s "[rng] $1$ATT" $mailddrs -a "$@" > /dev/null 2>&1 &&\
         echo -e "\n${gre}Wysłałem wiadomość do ${normal}${bold}$mailddrs${normal}${gre} z następującymi załącznikami:\n${whibg}${blafg}${bold}$(echo $@ | tr ' ' '\n')${normal}\n"
 fi
 
