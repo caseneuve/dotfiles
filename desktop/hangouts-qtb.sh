ca390ceb5990519bcfc1b66347697e26ecf9e1d8
caseneuve <piotr.kaznowski@gmail.com>
2019-01-18 20:59:33 +0100

    hangouts desktop added

---
+++ desktop/hangouts-qtb.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+# Path:        ~/.dotfiles/desktop/hangouts-qtb.sh
+# Created:     18.01.19, 20:55    @lenovo
+# Last update: 18.01.19, 20:56:12 @lenovo
+
+# >> DOC: 
+# Run hangouts in a separate window of a qutebrowser instance
+# May be run with rofi (check messenger.desktop)
+
+# >> RUN: 
+qutebrowser :command ":open -w https://hangouts.google.com"
