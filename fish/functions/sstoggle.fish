8915949cae2c6031727110bc1ca560ebd4ec7c4e
caseneuve <piotr.kaznowski@gmail.com>
2019-01-23 17:59:33 +0100

    DPMS disable added

--- fish/functions/sstoggle.fish
+++ fish/functions/sstoggle.fish
@@ -1,9 +1,10 @@
 function sstoggle -d "Toggle screen saver (DPMS)"
-    if test (xset -q | rg timeout | awk '{print $2}') -gt 0
-        command xset s off
-        echo Screen saver turned off.
+    if test (xset -q | rg timeout | awk '{print $2}') -gt 0 \
+        || test (string match -r "Enabled" (xset -q | rg " DPMS" | awk '{print $3}'))
+        command xset s off -dpms
+        echo "Screen saver turned off and DPMS has been disabled."
     else
-        command xset s on
-        echo "Screen saver set to" (math (xset -q | rg timeout | awk '{print $2}') / 60) minutes.
+        command xset s on +dpms
+        echo "Screen saver set to" (math (xset -q | rg timeout | awk '{print $2}') / 60) "minutes and DPMS has been enabled."
     end
 end
