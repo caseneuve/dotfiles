6753dc191293a836d323cbf8f412c16fc8c07bc8
caseneuve <piotr.kaznowski@gmail.com>
2019-02-20 14:56:16 +0100

    set local FZF opts for tmux

--- bin/tmux-starter.sh
+++ bin/tmux-starter.sh
@@ -2,12 +2,13 @@
 
 # Path:        ~/.tmux/tmux-hg.sh
 # Created:     13.03.18, 10:30    @x200
-# Last update: 24.11.18, 22:09:07 @x200
+# Last update: 20.02.19, 14:09:39 @x200
 
 # Doc: starts tmux with 3 windows, the second one named "hg" with HANGUPS and the third "cal" with CALCURSE running
 # https://stackoverflow.com/questions/5609192/how-to-set-up-tmux-so-that-it-starts-up-with-specified-windows-opened
 
 if [[ ! $(tmux ls | grep tmux ) ]]; then
+    FZF_DEFAULT_OPTS='--height 100% --layout=default --inline-info --border --margin 1,5%'
     tmux new-session -s 'tmux' -d
     source ~/.virtualenvs/weechatpy2/bin/activate
     #    tmux new-window -n 'hg' 'hangups'
