7713d5ad0d2dfa85f73555c4c86e55438d13ecc5
caseneuve <piotr.kaznowski@gmail.com>
2019-02-21 20:47:56 +0100

    run weechat from the package

--- bin/tmux-starter.sh
+++ bin/tmux-starter.sh
@@ -2,7 +2,7 @@
 
 # Path:        ~/.tmux/tmux-hg.sh
 # Created:     13.03.18, 10:30    @x200
-# Last update: 20.02.19, 14:09:39 @x200
+# Last update: 21.02.19, 19:53:33 @x200
 
 # Doc: starts tmux with 3 windows, the second one named "hg" with HANGUPS and the third "cal" with CALCURSE running
 # https://stackoverflow.com/questions/5609192/how-to-set-up-tmux-so-that-it-starts-up-with-specified-windows-opened
@@ -10,7 +10,7 @@
 if [[ ! $(tmux ls | grep tmux ) ]]; then
     FZF_DEFAULT_OPTS='--height 100% --layout=default --inline-info --border --margin 1,5%'
     tmux new-session -s 'tmux' -d
-    source ~/.virtualenvs/weechatpy2/bin/activate
+    # source ~/.virtualenvs/weechatpy2/bin/activate
     #    tmux new-window -n 'hg' 'hangups'
     tmux new-window -n 'wee' 'weechat'
     tmux new-window -n 'rng' 'ranger'
