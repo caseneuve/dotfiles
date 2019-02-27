71cdacb35f280de52e70267b5dc0f2e204be2d83
caseneuve <piotr.kaznowski@gmail.com>
2019-02-27 18:19:44 +0100

    added i3colors-dark & rainbow mode enable

--- i3/variables/i3colors-light.py
+++ i3/variables/i3colors-light.py
@@ -1,8 +1,8 @@
-#!/bin/python 
+# -*- eval:(rainbow-mode)  -*-
 
 # Path:        ~/git/hub/i3/i3colors.py
 # Created:     2019-02-27, 17:25    @x200
-# Last update: 2019-02-27, 18:16:23 @x200
+# Last update: 2019-02-27, 18:19:10 @x200
 
 # >> VARIABLES:
 i3colors = {
