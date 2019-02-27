c96bc151b71a52530bb47cc52fc591e62b2127eb
caseneuve <piotr.kaznowski@gmail.com>
2019-02-27 18:03:10 +0100

    variables dir added

---
+++ i3/variables/i3colors-light.py
@@ -0,0 +1,10 @@
+#!/bin/python 
+
+# Path:        ~/git/hub/i3/i3colors.py
+# Created:     2019-02-27, 17:25    @x200
+# Last update: 2019-02-27, 17:55:42 @x200
+
+# >> VARIABLES:
+i3colors = {
+    "i3bar_focused_txt": "#ececec",
+}
