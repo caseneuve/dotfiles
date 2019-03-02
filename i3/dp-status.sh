c63c440dadf576a67df76c8f5475321bf6d4e83e
caseneuve <piotr.kaznowski@gmail.com>
2019-03-02 23:07:32 +0100

    don't abuse cat!

--- i3/dp-status.sh
+++ i3/dp-status.sh
@@ -2,11 +2,11 @@
 
 # Path:        ~/.dotfiles/i3/dp-status.sh
 # Created:     2019-02-16, 09:49:23 @x200
-# Last update: 2019-03-02, 22:20:28 @x200
+# Last update: 2019-03-02, 23:06:47 @x200
 # Doc:         alert when dropbox server isn't running
 
 GLYPH=""
-FGCOLOR=$(cat $HOME/.Xresources | awk '/^*urgent/ {print $2}')
+FGCOLOR=$(awk '/^*urgent/ {print $2}' $HOME/.Xresources)
 
 [[ -n $(pgrep dropbox) ]] || echo "<span color='$FGCOLOR'>$GLYPH </span>"
 
