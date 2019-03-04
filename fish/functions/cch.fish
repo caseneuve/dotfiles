aad20cc5ef51a2b19eb32b37dcea8a6be30ede19
caseneuve <piotr.kaznowski@gmail.com>
2019-03-05 00:30:54 +0100

    notify send added

--- fish/functions/cch.fish
+++ fish/functions/cch.fish
@@ -1,4 +1,5 @@
 function cch -d "copy command from history selected with fzf"
     set cmd (history | fzf)
     echo $cmd | xclip -selection clipboard
+    notify-send "Fish" "<i>$cmd</i><br>copied to clipboard"
 end
