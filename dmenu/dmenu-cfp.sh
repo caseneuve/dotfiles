af5f8faa6bfd9d5fbd0ceae991bcbd225cd27193
caseneuve <piotr.kaznowski@gmail.com>
2019-03-05 12:54:52 +0100

    updt after new dmenu build

--- dmenu/dmenu-cfp.sh
+++ dmenu/dmenu-cfp.sh
@@ -2,24 +2,19 @@
 
 # Path:        ~/.dotfiles/dmenu/dmenu-cfp.sh
 # Created:     2019-03-04, 23:40    @x200
-# Last update: 2019-03-05, 00:28:26 @x200
+# Last update: 2019-03-05, 12:42:30 @x200
 # Doc:         select file and copy it's path to clipboard
 
-ARGS="-i -nb #0a0c10 -nf #ececec -sb #ffb142 -sf #191f26 -l 10"
+ARGS="-i -l 10"
+DIRS="$HOME/box $HOME/pdf $HOME/dwl"
+dir=$(fd -H -t f -t d . $DIRS |\
+          sed 's|/home/piotr/||' | sort |\
+          dmenu -p "Choose file" $ARGS)
 
-dir=$(fd -H -t d -d 4 . ~ | sed 's|/home/piotr/||' | sort | dmenu -p "Choose dir" -fn "Iosevka Light:size=10" $ARGS )
-
-if [[ -d "$HOME/$dir" && "$dir" != "" ]]
+if [[ -f "$HOME/$dir" ]]
 then
-    file=$(fd -H . "$dir" | sed 's@'"$dir"'@@' | sort | dmenu -p "Select in: ~/$dir" -fn "Iosevka Light:size=10" $ARGS)
-else
-    exit
-fi
-
-if [[ -f "$HOME/$dir$file" ]]
-then
-    echo "$HOME/$dir$file" | xclip -selection clipboard
-    file=$(basename "$file")
+    echo "$HOME/$dir" | xclip -selection clipboard
+    file=$(basename "$dir")
     notify-send "Dmenu"\
                 "<br><i>$file</i><br><br>path copied to clipboard"
 fi
--- dmenu/dmenu-choose-script.sh
+++ dmenu/dmenu-choose-script.sh
@@ -2,12 +2,13 @@
 
 # Path:        ~/.dotfiles/dmenu/dmenu-choose-script.sh
 # Created:     2019-03-04, 21:13    @x200
-# Last update: 2019-03-04, 22:55:25 @x200
-# Doc:
+# Last update: 2019-03-05, 12:30:56 @x200
+# Doc:         show scripts to execute in dmenu
 
 SCRIPT_DIR=$HOME/.config/dmenu/scripts
+ARGS="-i"
 
-script=$(ls $SCRIPT_DIR | dmenu -p "Execute" -nb "#0a0c10" -nf "#ececec" -sb "#ffb142" -sf "#191f26" -fn "Iosevka Light:size=10")
+script=$(ls $SCRIPT_DIR | dmenu -p "Execute" $ARGS)
 
 $SCRIPT_DIR/$script
 
