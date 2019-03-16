c03a1f01fb5fdb853cb5ea98e3c33afee82d59db
caseneuve <piotr.kaznowski@gmail.com>
2019-03-16 21:09:39 +0100

    color from Xres, mouse click

--- i3/scripts/dp-status.sh
+++ i3/scripts/dp-status.sh
@@ -1,6 +1,11 @@
 #!/bin/bash
 
 GLYPH=""
-FGCOLOR="#ff5252"
+FGCOLOR=$(awk '/^*urgent/ {print $2}' $HOME/.Xresources)
 
 [[ -n $(pgrep dropbox) ]] || echo "<span color='$FGCOLOR'> $GLYPH </span>"
+
+case $BLOCK_BUTTON in
+    1) pkill -RTMIN=12 i3blocks ;;
+    3) notify-send "Dropbox" "$(dropbox-cli status)" ;;
+esac
