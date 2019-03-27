55fdf7f2f5ae2b7c8ff146579ebacfdf28d85ffc
caseneuve <piotr.kaznowski@gmail.com>
2019-03-27 18:07:25 +0100

    refact.

--- bin/screencast.sh
+++ bin/screencast.sh
@@ -1,54 +1,37 @@
 #!/bin/bash
 
-# Last update: 2019-03-02, 21:25:36 @x200
-# Doc:         requires i3move (i3ipc installed via pip)
+# Last update: 2019-03-27, 18:00:46 @lenovo
+# Doc:          
+# Requires:    i3move; ffmpeg
+# Log:         25/08/2018, send signal 9 to i3blocks → i3screencast-indicator.sh
+#              24/08/2018, update: grab only active screen
+#              24/08/2018, bug: with kbd it grabs always primary screen
 
-# >> LOG: 
-# 25/08/2018 send signal 9 to i3blocks → i3screencast-indicator.sh
-# 24/08/2018 update: grab only active screen (todo: check how does it work with one screen)
-# 24/08/2018: bug: with kbd it grabs always primary screen (why?)
-
-# >> set output directory, if doesn't exist - create one:
 [[ -n $DISPLAY ]] || $DISPLAY=:0
-    
 CASTDIR=$HOME/wid/cast
 [[ -d $CASTDIR ]] || mkdir -p $CASTDIR
 
-# >> set ffmpeg parameters:
-DIMS=$(xrandr | grep "$(i3move -i | rg Output | awk '{print $2}')" | grep -o "[0-9]*x[0-9]*+[0-9]*+[0-9]*")
+DIMS=$(
+    xrandr | grep "$(i3move -i | rg Output | awk '{print $2}')" |\
+        grep -o "[0-9]*x[0-9]*+[0-9]*+[0-9]*"
+    )
 SCREEN_DIMS=${DIMS%%+[0-9]*}
 SCREEN_POS=$(echo ${DIMS#$SCREEN_DIMS} | tr '\+' ' ')
 SCR_X=$(echo $SCREEN_POS | awk '{print $1}')
-SCR_Y=$(echo $SCREEN_POS | awk '{print $2}')
-# @old:
-#dimensions=$(xdpyinfo | grep 'dimensions:' | awk '{print $2}')
 
-# >> set output file name:
 if [ -n "$1" ]; then
     outFile="$CASTDIR/$1.mkv"
 else
     outFile="$CASTDIR/out.mkv"
 fi
 
-# >> run
-# 1. kill compton (to avoid glithes, todo: can we fix this?)
-# if [[ $(pgrep grep compton) ]]; then
-#     pkill compton;
-#     COM=1
-# fi
+for ((i=3; i>0; i--)); do
+    notify-send "screencast" "start in $i sec."
+    sleep 1 && xdotool key ctrl+space
+done
 
-# 2. countdown:
-notify-send "screencast" "start in 3 sec."
-sleep 1 && xdotool key ctrl+space 
-notify-send "screencast" "start in 2 sec."
-sleep 1 && xdotool key ctrl+space
-notify-send "screencast" "start in 1 sec."
-sleep 1 && xdotool key ctrl+space
-
-# 3. run ffmpeg
 pkill -RTMIN+9 i3blocks
 ffmpeg -f x11grab -s "$SCREEN_DIMS" -i $DISPLAY.0+$SCR_X,$SCR_Y "$outFile"
 
-# 4.
+pkill screenkey
 pkill -RTMIN+9 i3blocks
-
