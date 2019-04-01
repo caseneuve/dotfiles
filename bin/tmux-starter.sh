2480d43c2f308e2aefec066aa1760ed6b08a5f61
caseneuve <piotr.kaznowski@gmail.com>
2019-04-01 23:00:18 +0200

    exprt EDITOR

--- bin/tmux-starter.sh
+++ bin/tmux-starter.sh
@@ -2,13 +2,14 @@
 
 # Path:        ~/.tmux/tmux-hg.sh
 # Created:     13.03.18, 10:30    @x200
-# Last update: 21.02.19, 19:53:33 @x200
+# Last update: 2019-04-01, 22:05:35 @toshiba
 
 # Doc: starts tmux with 3 windows, the second one named "hg" with HANGUPS and the third "cal" with CALCURSE running
 # https://stackoverflow.com/questions/5609192/how-to-set-up-tmux-so-that-it-starts-up-with-specified-windows-opened
 
 if [[ ! $(tmux ls | grep tmux ) ]]; then
     FZF_DEFAULT_OPTS='--height 100% --layout=default --inline-info --border --margin 1,5%'
+    EDITOR='emacsclient -nw -s term'
     tmux new-session -s 'tmux' -d
     # source ~/.virtualenvs/weechatpy2/bin/activate
     #    tmux new-window -n 'hg' 'hangups'
