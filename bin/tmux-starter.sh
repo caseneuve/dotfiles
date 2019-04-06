16883720caedc69d1f0d3bc8a00372ea79969fe0
caseneuve <piotr.kaznowski@gmail.com>
2019-04-06 14:51:12 +0200

    start mail with tmux

--- bin/tmux-starter.sh
+++ bin/tmux-starter.sh
@@ -2,7 +2,7 @@
 
 # Path:        ~/.tmux/tmux-hg.sh
 # Created:     13.03.18, 10:30    @x200
-# Last update: 2019-04-01, 22:05:35 @toshiba
+# Last update: 2019-04-06, 10:28:52 @x200
 
 # Doc: starts tmux with 3 windows, the second one named "hg" with HANGUPS and the third "cal" with CALCURSE running
 # https://stackoverflow.com/questions/5609192/how-to-set-up-tmux-so-that-it-starts-up-with-specified-windows-opened
@@ -18,6 +18,7 @@ if [[ ! $(tmux ls | grep tmux ) ]]; then
     tmux new-window -n 'cal' 'calcurse'
     tmux new-window -n 'mocp' 'mocp'
     tmux new-window -n 'htop' 'htop'
+    tmux new-window -n 'mail' 'neomutt'
     tmux rename-window -t%0 'term'
     tmux select-window -t 'tmux:term'
     tmux splitw -h -p 20
