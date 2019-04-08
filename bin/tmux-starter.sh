a6515a9db192f64275b4790d66bbf75d8d496e03
caseneuve <piotr.kaznowski@gmail.com>
2019-04-08 22:05:19 +0200

    htop as last

--- bin/tmux-starter.sh
+++ bin/tmux-starter.sh
@@ -2,7 +2,7 @@
 
 # Path:        ~/.tmux/tmux-hg.sh
 # Created:     2018-03-13, 10:30    @x200
-# Last update: 2019-04-06, 15:19:33 @x200
+# Last update: 2019-04-08, 22:00:54 @lenovo
 # Doc:         start tmux with apps: simple terminal window, weechat, ranger, calcurse, mocp, htop and neomutt
 #              or attach to existing instance
 # https://stackoverflow.com/questions/5609192/how-to-set-up-tmux-so-that-it-starts-up-with-specified-windows-opened
@@ -16,8 +16,8 @@ if [[ ! $(tmux ls | grep tmux ) ]]; then
     tmux new-window -n 'rng' 'ranger'
     tmux new-window -n 'cal' 'calcurse'
     tmux new-window -n 'mocp' 'mocp'
-    tmux new-window -n 'htop' 'htop'
     tmux new-window -n 'mail' 'neomutt'
+    tmux new-window -n 'htop' 'htop'
     tmux rename-window -t%0 'term'
     tmux select-window -t 'tmux:term'
     tmux splitw -h -p 20
