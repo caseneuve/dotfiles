6badd03c80ebd13c422d7c93800020c0f5d6fa34
caseneuve <piotr.kaznowski@gmail.com>
2019-04-20 18:11:08 +0200

    mutt cleanup cd.

--- mutt/viewpdf.sh
+++ mutt/viewpdf.sh
@@ -1,12 +1,8 @@
 #!/bin/bash
 
-# Last update: 17.08.18, 14:45:17 @x200
+# Last update: 2019-04-20, 18:09:48 @lenovo
+# Doc:         open pdf in mutt
 
-# Doc: skrypt wykonawczy dla mutt'a
-
-WS_PDF=$(i3-msg -t get_config | grep 'set \$ws5' | awk '{print $3 " " $4}')
-WS_MAIL=$(i3-msg -t get_config | grep 'set \$ws10' | awk '{print $3 " " $4}')
-
+WS_PDF=$(i3-msg -t get_config | grep 'set \$ws5' | awk '{print $3 " " $5}')
 i3-msg -q "$WS_PDF" && mupdf "$1" 
-i3-msg -q "$WS_MAIL"
 
