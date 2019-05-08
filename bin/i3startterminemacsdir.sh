8296316d7b7240cf41afe4c983cb663badad40d2
caseneuve <piotr.kaznowski@gmail.com>
2019-05-08 12:08:30 +0200

    adjust font to output size; preamble

--- bin/i3startterminemacsdir.sh
+++ bin/i3startterminemacsdir.sh
@@ -1,31 +1,34 @@
 #!/bin/bash
 
-#* Path:        ~/biu/bash/cdemacsdir.sh
+#* Path:        ~/.dotfiles/bin/i3startterminemacsdir.sh
 #  Created:     26.08.18, 15:00    @x200
-#  Last update: 2019-05-04, 21:09:30 @x200
-#  Doc:
-#* Requires:
-# - i3get.py → git: py-exercises (26/08/2018)
-# - i3ipc as dep.
-# - wmctrl
-# If an Emacs window is focused with a file in active buffer,
-# open the terminal in the working directory; 
-# else, open in $HOME.
+#  Last update: 2019-05-08, 12:08:20 @lenovo
+#  Doc:         If an Emacs window is focused with a file in active buffer,
+#               open the terminal in the working directory; 
+#               else, open in $HOME.
+#* Requires:    i3getgo
 #* Code:
 
 [[ -n $1 ]] && EXEC="-e $1" || EXEC=
 
-[[ -z $GOTPATH ]] && GOPATH=$HOME/go
-GET=$GOPATH/bin/i3getgo
+[[ -z $GOBIN ]] && GOBIN=$HOME/go/bin
+GET=$GOBIN/i3getgo
 
-if [[ $($GET -c) =~ "Emacs" ]]; then
-    FULL=$($GET -t)
-    FILE=$(basename "$FULL")
-    DIR=${FULL%$FILE}
+class=$($GET -c)
+out=$($GET -O)
+primary=$($GET -P)
+title=$($GET -t)
+
+# set bigger font for external output
+[[ $out != $primary ]] && FONT="-f monospace:size=14" || FONT=
+
+if [[ $class =~ "Emacs" ]]; then
+    FILE=$(basename "$title")
+    DIR=${title%$FILE}
     [[ -d $DIR ]] && cd $DIR
-    st -c term -t "st @$(date +%H:%M:%S)" $EXEC &
+    st -c term -t "st @$(date +%H:%M:%S)" $FONT $EXEC &
 else
-    [[ $($GET -c) == term ]] && DIR=$($GET -t | awk '{print $3}') || DIR=$PWD
+    [[ $class == term ]] && DIR=$(echo $title | awk '{print $NF}') || DIR=$PWD
     cd $DIR
-    st -c term -t "st @$(date +%H:%M:%S)" $EXEC &
+    st -c term -t "st @$(date +%H:%M:%S)" $FONT $EXEC &
 fi
