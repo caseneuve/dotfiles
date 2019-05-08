33a86cb16de69b8ca8e76d13c5c1d24317df5ea8
caseneuve <piotr.kaznowski@gmail.com>
2019-05-08 16:14:55 +0200

    add simple completions for venv

--- fish/functions/venv.fish
+++ fish/functions/venv.fish
@@ -1,7 +1,7 @@
 function venv -d 'create, activate or deactivate python virtualenv'
     set VENV_DIR "$HOME/.virtualenvs"
     switch $argv[1]
-        case new
+        case -n --new
             set path "$VENV_DIR/$argv[2]"
             if test -d "$path"
                 echo "Venv already set up!"
@@ -10,7 +10,7 @@ function venv -d 'create, activate or deactivate python virtualenv'
                 python3 -m venv $path \
                 && echo "Virtual env created at $path"
             end
-        case req
+        case -r --requirements
             if test $VIRTUAL_ENV
                 if test $argv[2]
                     set reqs "$argv[2]"
@@ -19,25 +19,31 @@ function venv -d 'create, activate or deactivate python virtualenv'
                 end
                 pip install -r "$reqs"
             else
-                echo -e "Venv not active.\nType `venv [name]` to activate it."
+                echo -e "Venv not active.\nType `venv -a [name]` to activate it."
             end
-        case d deactivate
+        case -d --deactivate
             if test $VIRTUAL_ENV
                 deactivate
             else
                 echo "Venv not active."
             end
-        case list
+        case -l --list
             for dir in (ls $VENV_DIR)
                 echo "$dir"
             end
-        case '*'
-            set path "$VENV_DIR/$argv[1]"
+        case -a --activate
+            set path "$VENV_DIR/$argv[2]"
             if test -d "$path"
                 source $path/bin/activate.fish
             else
-                echo -e "Venv $argv[1] doesn't exist.\nType: `venv new $argv[1]` to set it up."
+                echo -e "Venv '$argv[2]' doesn't exist.\nType: `venv -n $argv[2]` to set it up."
             end
     end
 
 end
+
+complete -c venv -s r -l requirements -x -a '(fd -e txt .)' -d 'install requirements'
+complete -c venv -s n -l new -x -a '(basename (pwd))' -d 'new venv'
+complete -c venv -s l -l list -x -d 'list venvs'
+complete -c venv -s d -l deactivate -x -d 'deactivate current venv'
+complete -c venv -s a -l activate -x -a '(ls ~/.virtualenvs)' -d 'activate venv'
