b0ad7c7e092b5ec3cf392731701fd37ea47542af
caseneuve <piotr.kaznowski@gmail.com>
2019-05-14 06:56:52 +0200

    polybar config

---
+++ polybar/polystart.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+# Terminate already running bar instances
+killall -q polybar
+
+# Wait until the processes have been shut down
+while pgrep -u $UID -x polybar >/dev/null; do sleep 1; done
+
+# Get monitor names
+PRIMARY=$(xrandr -q | awk '/connected primary/ {print $1}')
+SECONDARY=$(xrandr -q | awk '/ connected [0-9]/ {print $1}')
+
+# Launch bar1 and bar2
+MONITOR=$PRIMARY polybar -r primary &
+ln -sf /tmp/polybar_mqueue.$! /tmp/ipc-primary
+
+EXTERNAL=$SECONDARY polybar -r secondary &
+ln -sf /tmp/polybar_mqueue.$! /tmp/ipc-secondary
+
+#notify-send "polybar" "Bars launched..."
