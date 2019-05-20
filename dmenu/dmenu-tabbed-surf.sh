199e607b430ac121891504fee8eeb035d7ef6ef3
caseneuve <piotr.kaznowski@gmail.com>
2019-05-20 18:04:49 +0200

    add surf support

---
+++ dmenu/dmenu-tabbed-surf.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+# Path:        ~/.dotfiles/dmenu/dmenu-tabbed-surf.sh
+# Created:     2019-05-20, 17:16    @x200
+# Last update: 2019-05-20, 17:19:10 @x200
+# Doc:
+
+tabbed -c surf -e & disown
