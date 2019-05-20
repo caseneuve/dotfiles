c0b73d1d56469e8cf7bdac082a443102a5c2e0b0
caseneuve <piotr.kaznowski@gmail.com>
2019-05-20 23:13:38 +0200

    add lines

--- dmenu/dmenu-surf.sh
+++ dmenu/dmenu-surf.sh
@@ -2,11 +2,11 @@
 
 # Path:        ~/.dotfiles/dmenu/dmenu-surf.sh
 # Created:     2019-05-20, 17:23    @x200
-# Last update: 2019-05-20, 17:46:06 @x200
+# Last update: 2019-05-20, 18:45:38 @x200
 # Doc:
 
 BOOKMARKS=~/.surf/bookmarks.txt
 
-[[ -n $BOOKMARKS ]] && opt=$(sort $BOOKMARKS | dmenu -p "surf: ") || opt=$(dmenu -p "surf: ")
+[[ -n $BOOKMARKS ]] && LINES=$(wc -l < $BOOKMARKS) opt=$(sort $BOOKMARKS | dmenu -p "surf: " -l $LINES) || opt=$(dmenu -p "surf: ")
 
 [[ -n $opt ]] && surf https://$opt & disown
