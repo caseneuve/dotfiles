02db3b9eb5b74d432daed011c3a2cb7a795c8f62
caseneuve <piotr.kaznowski@gmail.com>
2019-05-31 12:16:30 +0200

    wintitle to the left, minor color changes

--- polybar/poly-diskinfo.sh
+++ polybar/poly-diskinfo.sh
@@ -2,14 +2,14 @@
 
 # Path:        ~/.dotfiles/polybar/filesystem.sh
 # Created:     2019-05-14, 17:01    @x200
-# Last update: 2019-05-14, 17:41:10 @x200
+# Last update: 2019-05-30, 19:46:46 @toshiba
 # Doc:
 
 HOME_PART_ROOT="/dev/sda"
 
 case $(hostname) in
     x200) HOME_PART=$HOME_PART_ROOT"4" ;;
-    lenovo) HOME_PART=$HOME_PART_ROOT"2" ;;
+    lenovo|toshiba) HOME_PART=$HOME_PART_ROOT"2" ;;
 esac
 
 df -h | awk '$0~v { gsub("G",""); print $4}' v=$HOME_PART
