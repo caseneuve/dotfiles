7dccaf2a70d3028f15b06d3a1de921054331e449
caseneuve <piotr.kaznowski@gmail.com>
2019-07-29 14:00:49 +0200

    use flags in i3startterminemacsdir for st exec and font

--- bin/i3startterminemacsdir.sh
+++ bin/i3startterminemacsdir.sh
@@ -2,14 +2,20 @@
 
 #* Path:        ~/.dotfiles/bin/i3startterminemacsdir.sh
 #  Created:     26.08.18, 15:00    @x200
-#  Last update: 2019-05-13, 12:51:51 @lenovo
+#  Last update: 2019-07-29, 14:00:05 @x200
 #  Doc:         If an Emacs window is focused with a file in active buffer,
 #               open the terminal in the working directory; 
 #               else, open in $HOME.
 #* Requires:    i3getgo
 #* Code:
 
-[[ -n $1 ]] && EXEC="-e $1" || EXEC=
+while getopts ":e:f:s:" option; do
+    case "${option}" in
+        e) EXEC="-e ${OPTARG}";;
+        f) FONT="-f ${OPTARG}";;
+        s) FONT="-f monospace:size=${OPTARG}";;
+    esac
+done
 
 [[ -z $GOBIN ]] && GOBIN=$HOME/go/bin
 GET=$GOBIN/i3getgo
@@ -18,9 +24,6 @@ class=$($GET -c)
 out=$($GET -O)
 primary=$($GET -P)
 title=$($GET -t)
-
-# set bigger font for external output
-[[ $out != $primary ]] && FONT="-f monospace:size=14" || FONT=
 
 if [[ $class =~ "Emacs" ]]; then
     FILE=$(basename "$title")
