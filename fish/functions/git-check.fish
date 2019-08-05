4d904a6811f1f27119794091a14beef8a1ee7960
caseneuve <piotr.kaznowski@gmail.com>
2019-08-05 12:21:01 +0200

    add exclude opt to git-check.fish

--- fish/functions/git-check.fish
+++ fish/functions/git-check.fish
@@ -1,21 +1,48 @@
 function git-check -d "check git status for some projects"
+    # check args for -e flag
+    set -l options (fish_opt -s e -l exclude --required-val)
+    argparse -n git-check $options -- $argv
+    if test $status -ne 0; return 1; end
+
+    function porcelain
+        set -l porcelain (git sp)
+        if test -n "$porcelain"
+            string replace $HOME/ "" (pwd)
+            set porcelain (string replace -a "  " "\n " "$porcelain")
+            set porcelain (string replace -a " ?" "\n " "$porcelain")
+            echo -e "$porcelain"
+            echo
+        end
+    end
+
+    echo CHECKING GIT STATUS FOR CHOSEN REPOS
+    echo ------------------------------------
+
     for dir in $HOME/git/lab $HOME/git/hub $HOME/web/schole
         cd $dir
         for project in (ls)
             if test -d $project
-                cd $project
-                set -l porcelain (git sp)
-                if test "$porcelain" != ""
-                    string replace $HOME/ "" (pwd)
-                    set -l porcelain (string replace -a "  " "\n " "$porcelain")
-                    set -l porcelain (string replace -a " ?" "\n " "$porcelain")
-                    echo -e "$porcelain"
-                    echo
+                # if project was excluded, continue
+                if set -q _flag_exclude
+                    set -l excluded (string replace -a "," "\|" "$_flag_exclude")
+                    if echo $project | grep "$excluded" >/dev/null
+                        continue
+                    end
                 end
+                # else: print git status porcelain info
+                cd $project
+                porcelain
                 cd ..
             end
         end
     end
+
+    cd $HOME/.dotfiles
+    porcelain
+
+    echo ------------------------------------
+    echo ...DONE!
+
 end
 
 
