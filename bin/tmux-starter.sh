c50ea165b4da6487bc442e1fd66cd0e17a198a6c
caseneuve <piotr.kaznowski@gmail.com>
2019-08-06 11:11:00 +0200

    start tmux with neofetch :P

--- bin/tmux-starter.sh
+++ bin/tmux-starter.sh
@@ -2,7 +2,7 @@
 
 # Path:        ~/.tmux/tmux-hg.sh
 # Created:     2018-03-13, 10:30    @x200
-# Last update: 2019-05-29, 13:00:25 @x200
+# Last update: 2019-08-06, 10:48:57 @lenovo
 # Doc:         start tmux with apps: simple terminal window, weechat, ranger, calcurse, mocp, htop and neomutt
 #              or attach to existing instance
 # https://stackoverflow.com/questions/5609192/how-to-set-up-tmux-so-that-it-starts-up-with-specified-windows-opened
@@ -11,7 +11,6 @@ if [[ ! $(tmux ls | grep tmux ) ]]; then
     FZF_DEFAULT_OPTS='--height 100% --layout=default --inline-info --border --margin 1,5%'
     EDITOR='emacsclient -nw -s term'
     tmux new-session -s 'tmux' -d 'fish'
-    # source ~/.virtualenvs/weechatpy2/bin/activate
     tmux new-window -n 'wee' 'weechat'
     tmux new-window -n 'rng' 'ranger'
     tmux new-window -n 'cal' 'calcurse'
@@ -22,7 +21,7 @@ if [[ ! $(tmux ls | grep tmux ) ]]; then
     tmux select-window -t 'tmux:term'
     tmux splitw -h -p 20
     tmux clock-mode
-    tmux select-pane -L
+    tmux select-pane -L \; send-keys 'neofetch' C-m
     tmux -2 attach -d
 else
     tmux -2 attach -d
