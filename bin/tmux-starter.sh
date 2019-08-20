9c3745271c876b29a65f4bc800465a8809d059a0
caseneuve <piotr.kaznowski@gmail.com>
2019-08-20 14:27:24 +0200

    tmux minor changes

--- bin/tmux-starter.sh
+++ bin/tmux-starter.sh
@@ -2,7 +2,7 @@
 
 # Path:        ~/.tmux/tmux-hg.sh
 # Created:     2018-03-13, 10:30    @x200
-# Last update: 2019-08-06, 10:48:57 @lenovo
+# Last update: 2019-08-20, 14:07:25 @x200
 # Doc:         start tmux with apps: simple terminal window, weechat, ranger, calcurse, mocp, htop and neomutt
 #              or attach to existing instance
 # https://stackoverflow.com/questions/5609192/how-to-set-up-tmux-so-that-it-starts-up-with-specified-windows-opened
@@ -21,9 +21,7 @@ if [[ ! $(tmux ls | grep tmux ) ]]; then
     tmux select-window -t 'tmux:term'
     tmux splitw -h -p 20
     tmux clock-mode
-    tmux select-pane -L \; send-keys 'neofetch' C-m
-    tmux -2 attach -d
-else
-    tmux -2 attach -d
 fi
+
+tmux -2 attach -d
 
