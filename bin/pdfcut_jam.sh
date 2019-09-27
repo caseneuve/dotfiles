414fea1c7ab9922e514ca44790709561db3a1f8f
caseneuve <piotr.kaznowski@gmail.com>
2019-09-28 00:08:14 +0200

    refactoring

--- bin/pdfcut_jam.sh
+++ bin/pdfcut_jam.sh
@@ -2,23 +2,37 @@
 
 # Path:        ~/.dotfiles/bin/pdfcut_jam.sh
 # Created:     2018-10-18, 23:40    @lenovo
-# Last update: 2019-04-01, 21:58:52 @toshiba
+# Last update: 2019-09-26, 08:48:58 @lenovo
 # Doc:         intelligently cut pdfs (used in ranger)
 
-if pacman -Qs pdfjam > /dev/null ; then
-    # set output title
-    if [[ $# > 2 ]]; then
-        [[ "$3" == *".pdf" ]] && out="$3" || out="$3.pdf"
-    else
-        out="${1%.pdf}"_$2.pdf
-    fi
-    # check pdf orientation
-    opt=$(
-        pdfinfo "$1" |\
-            awk '/Page size:/ {if ($3 > $5) print "--landscape"}'
-       )
-    # run cut command
-    pdfjam "$1" "$2" -o "$out" $opt
+# if pacman -Qs pdfjam > /dev/null ; then
+#     # set output title
+#     if [[ $# > 2 ]]; then
+#         [[ "$3" == *".pdf" ]] && out="$3" || out="$3.pdf"
+#     else
+#         out="${1%.pdf}"_$2.pdf
+#     fi
+#     # check pdf orientation
+#     opt=$(
+#         pdfinfo "$1" |\
+#             awk '/Page size:/ {if ($3 > $5) print "--landscape"}'
+#        )
+#     # run cut command
+#     pdfjam "$1" "$2" -o "$out" $opt
+# else
+#     echo "pdfjam not installed" && exit 1
+# fi
+
+
+if [[ $# > 2 ]]; then
+    [[ "$3" == *".pdf" ]] && out="$3" || out="$3.pdf"
 else
-    echo "pdfjam not installed" && exit 1
+    out="${1%.pdf}"_$2.pdf
 fi
+# check pdf orientation
+opt=$(
+    pdfinfo "$1" |\
+        awk '/Page size:/ {if ($3 > $5) print "--landscape"}'
+   )
+# run cut command
+pdfjam "$1" "$2" -o "$out" $opt
