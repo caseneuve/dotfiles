0cbad3f9ffdf743ff7bb3ef4b4d5e039b8e4c2f9
caseneuve <piotr.kaznowski@gmail.com>
2019-10-02 10:04:21 +0200

    fixes conditionals in mocp script (?)

--- moc/mocp-poly.sh
+++ moc/mocp-poly.sh
@@ -3,7 +3,7 @@
 #* ---------------------------------------
 # Path:        ~/scr/mocp-poly.sh
 # Created:     2019-06-05, 21:32    @x200
-# Last update: 2019-06-07, 10:47:11 @toshiba
+# Last update: 2019-10-02, 08:36:50 @lenovo
 # Doc:         check mocp config and
 #              OnSongChange property
 #* ----------------------------------------
@@ -11,19 +11,19 @@
 file="$(mocp -Q %file)"
 socket=/tmp/polybar-ipc-primary
 
-if ! [[ $file = "" ]]; then
-    while [[ $(mocp -Q %file) = "$file" ]]
+if ! [[ $file == "" ]]; then
+    while [[ $(mocp -Q %file) == "$file" ]]
     do
         if [[ $(mocp -Q %state) == PLAY ]]; then
             echo hook:module/moc1 >> $socket
             prev=PLAY
             sleep 1
-        elif ! [[ $prev = PAUSE ]]; then
+        elif ! [[ $prev == PAUSE ]]; then
             echo hook:module/moc2 >> $socket
             prev=PAUSE
         fi
     done
 fi
 
-[[ $(mocp -Q %state) = STOP ]] && echo hook:module/moc3 >> $socket
+[[ $(mocp -Q %state) == STOP ]] && echo hook:module/moc3 >> $socket
 exit
