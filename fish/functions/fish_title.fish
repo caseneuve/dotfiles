2561901780b051cf8919d25984decb06f116ac2f
caseneuve <piotr.kaznowski@gmail.com>
2019-10-22 11:32:32 +0200

    adds venv info to fish title

--- fish/functions/fish_title.fish
+++ fish/functions/fish_title.fish
@@ -1,12 +1,8 @@
 function fish_title -d "Title for the terminal window: display last command"
-    # if test -n "$argv[1]"
-    #     set cmd "$argv[1]"
-    # else
-    #     if test -d "$history[1]"
-    #         set cmd "cd"
-    #     else
-    #         set cmd "$history[1]"
-    #     end
-    # end
-    printf "%s %s %s %s" (date '+%H:%M:%S') (pwd) \$ "$argv[1]"
+    if test $VIRTUAL_ENV
+        set -l venv (string replace "$HOME/.virtualenvs/" "" $VIRTUAL_ENV)
+        printf "(%s) %s %s %s %s" $venv (date '+%H:%M:%S') (pwd) \$ "$argv[1]"
+    else
+        printf "%s %s %s %s" (date '+%H:%M:%S') (pwd) \$ "$argv[1]"
+    end
 end
