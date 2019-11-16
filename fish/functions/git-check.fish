cbd868c743ee987317f066c791eafc0fa35c0f83
caseneuve <piotr.kaznowski@gmail.com>
2019-11-16 19:20:34 +0100

    reformats git-check.fish

--- fish/functions/git-check.fish
+++ fish/functions/git-check.fish
@@ -1,9 +1,9 @@
 function git-check -d "check git status for some projects"
     # parse args
     set -l options (fish_opt -s e -l exclude --required-val)
-    set options (fish_opt -s v -l verbose --optional-val) $options
-    set options (fish_opt -s s -l short --optional-val) $options
-    set options (fish_opt -s h -l help --optional-val) $options
+    set options    (fish_opt -s v -l verbose --optional-val) $options
+    set options    (fish_opt -s s -l short --optional-val) $options
+    set options    (fish_opt -s h -l help --optional-val) $options
     
     argparse -n git-check $options -- $argv
     if test $status -ne 0; return 1; end
@@ -27,14 +27,11 @@ Options:
     
     function porcelain -S
         set -l this (string replace $HOME "~" (pwd))
-
         set -l porcelain (git status --porcelain 2>/dev/null)
         if test $status -ne 0
             if set -q _flag_verbose
-                if $long
-                    printf "\n!! Not git repo: $this\n" 
-                else
-                    printf " [!!] $this *is not a git repo*\n" 
+                if $long; printf "\n!! Not git repo: $this\n" 
+                else;     printf " [!!] $this *is not a git repo*\n" 
                 end
             else
                 if $long; echo -n "#"; end
@@ -50,18 +47,16 @@ Options:
             else
                 printf "\n\n$this\n"
                 set porcelain (string replace -a "D  " " D " "$porcelain")
-                set porcelain (string replace -a "  " "\n " "$porcelain")
-                set porcelain (string replace -a " ?" "\n " "$porcelain")
+                set porcelain (string replace -a "  "  "\n " "$porcelain")
+                set porcelain (string replace -a " ?"  "\n " "$porcelain")
                 printf "$porcelain\n"
             end
         else
-            if not set -q _flag_short
-                printf .
-            end
+            if not set -q _flag_short; printf . ; end
         end
     end
 
-    echo "Checking git status for chosen repos:"
+    printf "Checking git status for chosen repos:"
 
     for dir in $HOME/git/* $HOME/web/schole
         cd $dir
@@ -70,14 +65,10 @@ Options:
                 # if project was excluded, continue
                 if set -q _flag_exclude
                     set -l excluded (string replace -a "," "\|" "$_flag_exclude")
-                    if echo $project | grep "$excluded" >/dev/null
-                        continue
-                    end
+                    if echo $project | grep "$excluded" >/dev/null; continue; end
                 end
                 # else: print git status porcelain info
-                cd $project
-                porcelain
-                cd ..
+                cd $project; porcelain; cd ..
             end
         end
     end
@@ -85,12 +76,7 @@ Options:
     cd $HOME/.dotfiles
     porcelain
 
-    # echo
-
     # cleanup global var
     set -e verbose
     set -e long
 end
-
-
-
