8141e6a5685ba06668e502a088acb295c2b1c824
caseneuve <piotr.kaznowski@gmail.com>
2019-11-24 16:42:43 +0100

    simplifies mocp polybar info

--- moc/mocp-poly.sh
+++ moc/mocp-poly.sh
@@ -3,27 +3,21 @@
 #* ---------------------------------------
 # Path:        ~/scr/mocp-poly.sh
 # Created:     2019-06-05, 21:32    @x200
-# Last update: 2019-10-02, 08:36:50 @lenovo
+# Last update: 2019-11-24, 16:41:00 @lenovo
 # Doc:         check mocp config and
 #              OnSongChange property
 #* ----------------------------------------
 
-file="$(mocp -Q %file)"
 socket=/tmp/polybar-ipc-primary
 
-if ! [[ $file == "" ]]; then
-    while [[ $(mocp -Q %file) == "$file" ]]
-    do
-        if [[ $(mocp -Q %state) == PLAY ]]; then
-            echo hook:module/moc1 >> $socket
-            prev=PLAY
-            sleep 1
-        elif ! [[ $prev == PAUSE ]]; then
-            echo hook:module/moc2 >> $socket
-            prev=PAUSE
-        fi
-    done
-fi
+while [ -e $socket ]; do
+    case "$(mocp -Q %state)" in
+        PLAY) echo hook:module/moc1 >> $socket;;
+        PAUSE) echo hook:module/moc2 >> $socket ;;
+        *) break ;;
+    esac
+    sleep 1
+done
 
-[[ $(mocp -Q %state) == STOP ]] && echo hook:module/moc3 >> $socket
+echo hook:module/moc3 >> $socket
 exit
